4cb8a5a1b574b9781036b93d80510f03aff67339
Julien <julien.etelain@outlook.com>
2020-05-31 17:47:20 +0200

    organisation des fichiers

---
+++ maps/tanoa.js
@@ -0,0 +1,10 @@
+﻿InitMap({
+    CRS: MGRS_CRS(0.01385, 0.01385, 213),
+    tilePattern: '/maps/tanoa/{z}/{x}/{y}.png',
+    maxZoom: 5,
+    minZoom: 0,
+    defaultZoom: 2,
+    attribution: '&copy; Bohemia Interactive',
+    tileSize: 213,
+    center: [7000,7000]
+});
---
+++ maps/taunus.js
@@ -0,0 +1,10 @@
+﻿InitMap({
+    CRS: MGRS_CRS(0.013832, 0.013828, 284),
+    tilePattern: '/maps/taunus/{z}/{x}/{y}.png',
+    maxZoom: 5,
+    minZoom: 0,
+    defaultZoom: 2,
+    attribution: '&copy; Bohemia Interactive, X-Cam-Taunus team',
+    tileSize: 284,
+    center: [10000,10000]
+});
