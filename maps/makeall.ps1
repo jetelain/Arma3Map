e10c2296149eed6d6ebdcbe019965fee3f1b9da5
Julien <julien.etelain@outlook.com>
2020-10-27 13:09:59 +0100

    Fix encoding

--- maps/makeall.ps1
+++ maps/makeall.ps1
@@ -1,5 +1,5 @@
 Remove-Item $PSScriptRoot/all.js
-Get-Content -Path $PSScriptRoot/*.js| Set-Content $PSScriptRoot/all.js
+Get-Content -Path $PSScriptRoot/*.js  -Encoding UTF8 | Set-Content $PSScriptRoot/all.js -Encoding UTF8
 
 $js = Get-Content $PSScriptRoot/all.js
 $js = $js -replace 'Arma3Map\.Maps\.([a-z0-9_]+) *= *{', '$1:{'
@@ -9,7 +9,7 @@ $js = $js -replace '};', '},'
 $js[0] = '{'+$js[0];
 $js[$js.Length-1] = '}}';
 
-$js | Set-Content $PSScriptRoot/all.json
+$js | Set-Content $PSScriptRoot/all.json -Encoding UTF8
 
 $json = $js | ConvertFrom-Json
-$json | ConvertTo-Json -Depth 3 -Compress | Set-Content $PSScriptRoot/all.json
+$json | ConvertTo-Json -Depth 3 -Compress | Set-Content $PSScriptRoot/all.json -Encoding UTF8
