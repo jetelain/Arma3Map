3a965d9b8ab88c3bc0297531ce396cd33c6efed5
Karol <lokarcqaz@gmail.com>
2021-02-21 17:54:40 +0100

    mcn_aliabad

---
+++ maps/mcn_aliabad.js
@@ -0,0 +1,66 @@
+Arma3Map.Maps.mcn_aliabad = {
+  CRS: MGRS_CRS(0.063, 0.063, 323),
+  "worldName": "mcn_aliabad",
+  "tilePattern": "/maps/mcn_aliabad/{z}/{x}/{y}.png",
+  "maxZoom": 3,
+  "minZoom": 0,
+  "defaultZoom": 2,
+  "attribution": "&copy; Bohemia Interactive",
+  "tileSize": 323,
+  "center": [
+    2560,
+    2560
+  ],
+  "worldSize": 5120.0,
+  "preview": null,
+  "dlc": null,
+  "title": "Aliabad Region",
+  "steamWorkshop": null,
+  "cities": [
+    {
+      "name": "Darshag",
+      "x": 2591.23,
+      "y": 1827.63
+    },
+    {
+      "name": "Abad",
+      "x": 2195.79,
+      "y": 2901.67
+    },
+    {
+      "name": "Ahmad Golam",
+      "x": 1971.49,
+      "y": 2129.44
+    },
+    {
+      "name": "Tagaw",
+      "x": 3633.87,
+      "y": 2166.26
+    },
+    {
+      "name": "Katal",
+      "x": 2443.39,
+      "y": 3663.17
+    },
+    {
+      "name": "Dawlatsah",
+      "x": 1511.02,
+      "y": 2811.3
+    },
+    {
+      "name": "Wolak",
+      "x": 3765.13,
+      "y": 3253.84
+    },
+    {
+      "name": "Narmaki",
+      "x": 2810.1,
+      "y": 1096.49
+    },
+    {
+      "name": "Hadiz",
+      "x": 4133.88,
+      "y": 1454.22
+    }
+  ]
+};
