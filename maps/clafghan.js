baaa2c39ea709481bd213de0dc00b16e0eb4dc96
Karol <lokarcqaz@gmail.com>
2021-02-21 18:26:18 +0100

    clafghan

---
+++ maps/clafghan.js
@@ -0,0 +1,136 @@
+Arma3Map.Maps.clafghan = {
+  CRS: MGRS_CRS(0.01575, 0.01575, 323),
+  "worldName": "clafghan",
+  "tilePattern": "/maps/clafghan/{z}/{x}/{y}.png",
+  "maxZoom": 5,
+  "minZoom": 0,
+  "defaultZoom": 2,
+  "attribution": "&copy; Bohemia Interactive",
+  "tileSize": 323,
+  "center": [
+    10240,
+    10240
+  ],
+  "worldSize": 20480.0,
+  "preview": null,
+  "dlc": null,
+  "title": "CLA Clafghan",
+  "steamWorkshop": null,
+  "cities": [
+    {
+      "name": "Hysad",
+      "x": 9459.87,
+      "y": 12439.1
+    },
+    {
+      "name": "Etnikora",
+      "x": 9684.99,
+      "y": 14393.5
+    },
+    {
+      "name": "Spetzyahir",
+      "x": 13836.5,
+      "y": 8043.68
+    },
+    {
+      "name": "Gonzalabad",
+      "x": 9606.88,
+      "y": 15381.4
+    },
+    {
+      "name": "Lolosalam",
+      "x": 15610.2,
+      "y": 8991.99
+    },
+    {
+      "name": "Sulakhira",
+      "x": 10505.6,
+      "y": 15950.2
+    },
+    {
+      "name": "Yanotabir",
+      "x": 9646.43,
+      "y": 16449.7
+    },
+    {
+      "name": "Agresorán",
+      "x": 10483.3,
+      "y": 16737.0
+    },
+    {
+      "name": "Ovallestán",
+      "x": 12564.8,
+      "y": 16557.6
+    },
+    {
+      "name": "Scarzirah",
+      "x": 9478.47,
+      "y": 17026.4
+    },
+    {
+      "name": "Kenykmheni",
+      "x": 17154.1,
+      "y": 11352.2
+    },
+    {
+      "name": "Al_Jebel",
+      "x": 3962.49,
+      "y": 13656.8
+    },
+    {
+      "name": "Kepsora",
+      "x": 14198.1,
+      "y": 16898.3
+    },
+    {
+      "name": "Machifazar",
+      "x": 17086.3,
+      "y": 5960.12
+    },
+    {
+      "name": "Alkhemora",
+      "x": 6019.96,
+      "y": 17139.1
+    },
+    {
+      "name": "Demenazhir",
+      "x": 7855.74,
+      "y": 18082.1
+    },
+    {
+      "name": "Fareldraguz",
+      "x": 6188.59,
+      "y": 17413.3
+    },
+    {
+      "name": "Mandingara",
+      "x": 18553.9,
+      "y": 9114.7
+    },
+    {
+      "name": "Al_Riyal",
+      "x": 18527.5,
+      "y": 12533.7
+    },
+    {
+      "name": "Marmakur",
+      "x": 19548.4,
+      "y": 11809.3
+    },
+    {
+      "name": "Guyosabir",
+      "x": 7383.61,
+      "y": 19928.8
+    },
+    {
+      "name": "Mini Al Malak",
+      "x": 17496.0,
+      "y": 18142.8
+    },
+    {
+      "name": "Lotohafaz",
+      "x": 18713.1,
+      "y": 19266.8
+    }
+  ]
+};
