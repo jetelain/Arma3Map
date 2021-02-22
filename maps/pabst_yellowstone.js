cbee998014717c93234f2d75c106d57ce77599e8
Karol <lokarcqaz@gmail.com>
2021-02-22 04:27:59 +0100

    pabst_yellowstone

---
+++ maps/pabst_yellowstone.js
@@ -0,0 +1,66 @@
+Arma3Map.Maps.pabst_yellowstone = {
+  CRS: MGRS_CRS(0.0315, 0.0315, 323),
+  "worldName": "pabst_yellowstone",
+  "tilePattern": "/maps/pabst_yellowstone/{z}/{x}/{y}.png",
+  "maxZoom": 4,
+  "minZoom": 0,
+  "defaultZoom": 2,
+  "attribution": "&copy; Bohemia Interactive",
+  "tileSize": 323,
+  "center": [
+    5120,
+    5120
+  ],
+  "worldSize": 10240.0,
+  "preview": null,
+  "dlc": null,
+  "title": "Yellowstone",
+  "steamWorkshop": null,
+  "cities": [
+    {
+      "name": "Norris",
+      "x": 2992.56,
+      "y": 4565.64
+    },
+    {
+      "name": "Washburn",
+      "x": 6957.58,
+      "y": 3673.17
+    },
+    {
+      "name": "Grebe Prision",
+      "x": 7200.0,
+      "y": 5520.0
+    },
+    {
+      "name": "Brewery",
+      "x": 2984.85,
+      "y": 6626.1
+    },
+    {
+      "name": "Wapiti",
+      "x": 8202.88,
+      "y": 4180.27
+    },
+    {
+      "name": "Potato Research Facility",
+      "x": 1306.97,
+      "y": 4890.55
+    },
+    {
+      "name": "Solfatara",
+      "x": 3034.36,
+      "y": 7544.33
+    },
+    {
+      "name": "Seismic Research Station",
+      "x": 6047.64,
+      "y": 7900.44
+    },
+    {
+      "name": "Canyon",
+      "x": 9403.19,
+      "y": 4606.12
+    }
+  ]
+};
