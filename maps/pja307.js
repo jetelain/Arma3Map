afacdc7dcdedd3d6b6bc8bf83c853508ac3bcc05
Karol <lokarcqaz@gmail.com>
2021-02-22 01:25:27 +0100

    pja310
    G.O.S Dariyah

---
+++ maps/pja307.js
@@ -0,0 +1,146 @@
+Arma3Map.Maps.pja307 = {
+  CRS: MGRS_CRS(0.01575, 0.01575, 323),
+  "worldName": "pja307",
+  "tilePattern": "/maps/pja307/{z}/{x}/{y}.png",
+  "maxZoom": 5,
+  "minZoom": 0,
+  "defaultZoom": 2,
+  "attribution": "&copy; Bohemia Interactive",
+  "tileSize": 323,
+  "center": [
+    10240,
+    10240
+  ],
+  "worldSize": 20480.0,
+  "preview": null,
+  "dlc": null,
+  "title": "G.O.S Dariyah",
+  "steamWorkshop": null,
+  "cities": [
+    {
+      "name": "Fan El Hisn",
+      "x": 9134.0,
+      "y": 11458.0
+    },
+    {
+      "name": "Ben Guerrir",
+      "x": 11859.0,
+      "y": 8811.0
+    },
+    {
+      "name": "illizli",
+      "x": 8640.0,
+      "y": 10897.0
+    },
+    {
+      "name": "Bani Sanaad",
+      "x": 10635.0,
+      "y": 8394.0
+    },
+    {
+      "name": "Al Hariq",
+      "x": 12509.0,
+      "y": 8612.0
+    },
+    {
+      "name": "As Sayyani",
+      "x": 13684.0,
+      "y": 10676.0
+    },
+    {
+      "name": "Oum Ali",
+      "x": 13688.0,
+      "y": 11840.0
+    },
+    {
+      "name": "Waddan",
+      "x": 8938.0,
+      "y": 7694.0
+    },
+    {
+      "name": "Hun",
+      "x": 12687.0,
+      "y": 7679.0
+    },
+    {
+      "name": "Telagh",
+      "x": 13707.0,
+      "y": 13621.0
+    },
+    {
+      "name": "Anlif",
+      "x": 12521.0,
+      "y": 14722.0
+    },
+    {
+      "name": "Ghriss",
+      "x": 13611.0,
+      "y": 14459.0
+    },
+    {
+      "name": "Mizzdah",
+      "x": 7307.0,
+      "y": 6973.0
+    },
+    {
+      "name": "Bir Al Huffay",
+      "x": 15774.0,
+      "y": 12087.0
+    },
+    {
+      "name": "Kalaat",
+      "x": 12752.0,
+      "y": 5624.0
+    },
+    {
+      "name": "Tlidjen",
+      "x": 16400.0,
+      "y": 8934.0
+    },
+    {
+      "name": "Akka",
+      "x": 12043.0,
+      "y": 16893.0
+    },
+    {
+      "name": "Tidjikja",
+      "x": 6717.0,
+      "y": 16492.0
+    },
+    {
+      "name": "Haere Lao",
+      "x": 12798.0,
+      "y": 18278.0
+    },
+    {
+      "name": "Dehiba",
+      "x": 6994.0,
+      "y": 3269.0
+    },
+    {
+      "name": "Mal",
+      "x": 12043.0,
+      "y": 19186.0
+    },
+    {
+      "name": "Dhamar",
+      "x": 14850.0,
+      "y": 2849.0
+    },
+    {
+      "name": "Diminli",
+      "x": 5699.0,
+      "y": 18672.0
+    },
+    {
+      "name": "Jiblah",
+      "x": 18344.0,
+      "y": 2749.0
+    },
+    {
+      "name": "Al Minsah",
+      "x": 19402.0,
+      "y": 3730.0
+    }
+  ]
+};
