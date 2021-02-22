303e9256c69d09d47bd45880383076cfdf8692bf
Karol <lokarcqaz@gmail.com>
2021-02-22 01:58:09 +0100

    rhspkl

---
+++ maps/rhspkl.js
@@ -0,0 +1,111 @@
+Arma3Map.Maps.rhspkl = {
+  CRS: MGRS_CRS(0.0315, 0.0315, 259),
+  "worldName": "rhspkl",
+  "tilePattern": "/maps/rhspkl/{z}/{x}/{y}.png",
+  "maxZoom": 4,
+  "minZoom": 0,
+  "defaultZoom": 2,
+  "attribution": "&copy; Bohemia Interactive",
+  "tileSize": 259,
+  "center": [
+    4096,
+    4096
+  ],
+  "worldSize": 8192.0,
+  "preview": null,
+  "dlc": null,
+  "title": "Prei Khmaoch Luong",
+  "steamWorkshop": null,
+  "cities": [
+    {
+      "name": "Phoum Chek Yeak",
+      "x": 1303.45,
+      "y": 448.17
+    },
+    {
+      "name": " Chamkar Samraong",
+      "x": 2137.23,
+      "y": 1802.32
+    },
+    {
+      "name": "Chamkar Yeak",
+      "x": 3207.34,
+      "y": 1751.26
+    },
+    {
+      "name": "Kanleng Krohthnak",
+      "x": 1452.67,
+      "y": 4588.58
+    },
+    {
+      "name": "Kiri Preaek",
+      "x": 3178.48,
+      "y": 4009.21
+    },
+    {
+      "name": "Small River Village",
+      "x": 4143.78,
+      "y": 3200.5
+    },
+    {
+      "name": "Kanleng Sreuv Samnang",
+      "x": 4817.17,
+      "y": 3053.3
+    },
+    {
+      "name": "PsarKrom",
+      "x": 4547.48,
+      "y": 3653.8
+    },
+    {
+      "name": "Men Cheah ChabTrei",
+      "x": 5343.36,
+      "y": 2380.78
+    },
+    {
+      "name": "Phnom Santhuk Santheab",
+      "x": 1379.14,
+      "y": 5826.56
+    },
+    {
+      "name": "Koun Auloek",
+      "x": 3872.61,
+      "y": 4932.92
+    },
+    {
+      "name": "Phoumi moattonle",
+      "x": 5939.89,
+      "y": 1581.04
+    },
+    {
+      "name": "Plantation Sainte Clementine",
+      "x": 4549.78,
+      "y": 4649.57
+    },
+    {
+      "name": "Samlout Kiri",
+      "x": 920.17,
+      "y": 6296.16
+    },
+    {
+      "name": "Ou Malai Chek",
+      "x": 200.45,
+      "y": 6435.24
+    },
+    {
+      "name": "Amleang Leak",
+      "x": 2112.17,
+      "y": 6600.98
+    },
+    {
+      "name": "Chaeh Chab Trei",
+      "x": 6875.0,
+      "y": 1018.49
+    },
+    {
+      "name": "Phoum Srei Aphsok",
+      "x": 4319.83,
+      "y": 6804.02
+    }
+  ]
+};
