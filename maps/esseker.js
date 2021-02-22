fe90620402d65f6f1e7af262f70ab7dc60db9f85
Karol <lokarcqaz@gmail.com>
2021-02-22 02:52:57 +0100

    esseker

---
+++ maps/esseker.js
@@ -0,0 +1,181 @@
+Arma3Map.Maps.esseker = {
+  CRS: MGRS_CRS(0.0315, 0.0315, 388),
+  "worldName": "esseker",
+  "tilePattern": "/maps/esseker/{z}/{x}/{y}.png",
+  "maxZoom": 4,
+  "minZoom": 0,
+  "defaultZoom": 2,
+  "attribution": "&copy; Bohemia Interactive",
+  "tileSize": 388,
+  "center": [
+    6144,
+    6144
+  ],
+  "worldSize": 12288.0,
+  "preview": null,
+  "dlc": null,
+  "title": "Esseker",
+  "steamWorkshop": null,
+  "cities": [
+    {
+      "name": "Krupa",
+      "x": 1968.08,
+      "y": 4217.89
+    },
+    {
+      "name": "Petlov JunkYard",
+      "x": 4297.08,
+      "y": 3002.38
+    },
+    {
+      "name": "Kupres",
+      "x": 2680.06,
+      "y": 4688.45
+    },
+    {
+      "name": "Petlov Grad",
+      "x": 4554.97,
+      "y": 3598.76
+    },
+    {
+      "name": "Plomino Power Station",
+      "x": 5146.58,
+      "y": 4684.66
+    },
+    {
+      "name": "Kula",
+      "x": 4961.0,
+      "y": 5241.46
+    },
+    {
+      "name": "Danilov Grad",
+      "x": 3104.13,
+      "y": 6156.76
+    },
+    {
+      "name": "Plava Vrana Military Complex",
+      "x": 6655.22,
+      "y": 3908.51
+    },
+    {
+      "name": "Melina",
+      "x": 3564.79,
+      "y": 6982.9
+    },
+    {
+      "name": "Neptune Resort",
+      "x": 6433.36,
+      "y": 5525.88
+    },
+    {
+      "name": "Gulash",
+      "x": 6347.43,
+      "y": 6164.64
+    },
+    {
+      "name": "Rama",
+      "x": 2077.63,
+      "y": 7802.5
+    },
+    {
+      "name": "Weiss College",
+      "x": 7372.53,
+      "y": 5310.04
+    },
+    {
+      "name": "Lug",
+      "x": 6265.91,
+      "y": 6875.04
+    },
+    {
+      "name": "Focha",
+      "x": 5511.42,
+      "y": 7439.72
+    },
+    {
+      "name": "Chokory",
+      "x": 4860.2,
+      "y": 8080.81
+    },
+    {
+      "name": "Villa Victoria Hotel",
+      "x": 8387.77,
+      "y": 4721.22
+    },
+    {
+      "name": "Esseker Industrial Zone",
+      "x": 8301.15,
+      "y": 5684.96
+    },
+    {
+      "name": "Posestra",
+      "x": 7654.21,
+      "y": 6893.39
+    },
+    {
+      "name": "Old Esseker",
+      "x": 8680.38,
+      "y": 5473.91
+    },
+    {
+      "name": "ES Petrol",
+      "x": 4004.61,
+      "y": 9305.63
+    },
+    {
+      "name": "Adela Airfield",
+      "x": 9984.83,
+      "y": 4866.38
+    },
+    {
+      "name": "Camp Spencer",
+      "x": 1159.49,
+      "y": 10316.2
+    },
+    {
+      "name": "Nekrashuk",
+      "x": 4972.97,
+      "y": 10318.6
+    },
+    {
+      "name": "Tuk",
+      "x": 7153.41,
+      "y": 9348.54
+    },
+    {
+      "name": "Pilana Sawmill",
+      "x": 9014.6,
+      "y": 7945.77
+    },
+    {
+      "name": "Lower Esseker",
+      "x": 10722.8,
+      "y": 5503.34
+    },
+    {
+      "name": "Igman Military base",
+      "x": 7641.77,
+      "y": 10151.7
+    },
+    {
+      "name": "Chardak Resort",
+      "x": 10131.9,
+      "y": 7931.15
+    },
+    {
+      "name": "Borosh",
+      "x": 10010.1,
+      "y": 9941.86
+    },
+    {
+      "name": "Novi Grad",
+      "x": 11862.4,
+      "y": 8002.95
+    },
+    {
+      "name": "Gromada",
+      "x": 9963.7,
+      "y": 10880.0
+    }
+  ]
+};
