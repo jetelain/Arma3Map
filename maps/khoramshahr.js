528f063b5b596d1993e5762ff9fa7de1ad57d61a
Karol <lokarcqaz@gmail.com>
2021-02-22 01:48:13 +0100

    khoramshar

---
+++ maps/khoramshahr.js
@@ -0,0 +1,20 @@
+Arma3Map.Maps.khoramshahr = {
+  CRS: MGRS_CRS(0.063, 0.063, 323),
+  "worldName": "khoramshahr",
+  "tilePattern": "/maps/khoramshahr/{z}/{x}/{y}.png",
+  "maxZoom": 3,
+  "minZoom": 0,
+  "defaultZoom": 2,
+  "attribution": "&copy; Bohemia Interactive",
+  "tileSize": 323,
+  "center": [
+    2560,
+    2560
+  ],
+  "worldSize": 5120.0,
+  "preview": null,
+  "dlc": null,
+  "title": "Khoramshahr",
+  "steamWorkshop": null,
+  "cities": []
+};
