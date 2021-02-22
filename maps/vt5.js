67531c006fef684afea77c28dbb7ccc0f3c41ee2
Karol <lokarcqaz@gmail.com>
2021-02-22 02:27:51 +0100

    vt5
    Vt5 - Suomi Finland

---
+++ maps/vt5.js
@@ -0,0 +1,20 @@
+Arma3Map.Maps.vt5 = {
+  CRS: MGRS_CRS(0.063, 0.063, 379),
+  "worldName": "vt5",
+  "tilePattern": "/maps/vt5/{z}/{x}/{y}.png",
+  "maxZoom": 3,
+  "minZoom": 0,
+  "defaultZoom": 2,
+  "attribution": "&copy; Bohemia Interactive",
+  "tileSize": 379,
+  "center": [
+    2999,
+    2999
+  ],
+  "worldSize": 5999.62,
+  "preview": null,
+  "dlc": null,
+  "title": "Vt5 - Suomi Finland",
+  "steamWorkshop": null,
+  "cities": []
+};
