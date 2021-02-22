0b6991c5357071ece393da8e90ec6f88be903b99
Karol <lokarcqaz@gmail.com>
2021-02-22 02:58:02 +0100

    eden
    Everon

---
+++ maps/eden.js
@@ -0,0 +1,111 @@
+Arma3Map.Maps.eden = {
+  CRS: MGRS_CRS(0.01575, 0.01575, 202),
+  "worldName": "eden",
+  "tilePattern": "/maps/eden/{z}/{x}/{y}.png",
+  "maxZoom": 5,
+  "minZoom": 0,
+  "defaultZoom": 2,
+  "attribution": "&copy; Bohemia Interactive",
+  "tileSize": 202,
+  "center": [
+    6400,
+    6400
+  ],
+  "worldSize": 12800.0,
+  "preview": null,
+  "dlc": null,
+  "title": "Everon",
+  "steamWorkshop": null,
+  "cities": [
+    {
+      "name": "Chotain",
+      "x": 7057.0,
+      "y": 5987.0
+    },
+    {
+      "name": "Entre Deux",
+      "x": 5735.0,
+      "y": 7059.0
+    },
+    {
+      "name": "Provins",
+      "x": 5501.0,
+      "y": 6050.0
+    },
+    {
+      "name": "Laruns",
+      "x": 7556.0,
+      "y": 5558.0
+    },
+    {
+      "name": "Figari",
+      "x": 5283.0,
+      "y": 5375.0
+    },
+    {
+      "name": "Montignac",
+      "x": 4935.0,
+      "y": 6994.0
+    },
+    {
+      "name": "Levie",
+      "x": 7554.0,
+      "y": 4737.0
+    },
+    {
+      "name": "Gravette",
+      "x": 4116.64,
+      "y": 7753.69
+    },
+    {
+      "name": "Morton",
+      "x": 5159.0,
+      "y": 3943.0
+    },
+    {
+      "name": "Tyrone",
+      "x": 4946.0,
+      "y": 9056.0
+    },
+    {
+      "name": "Meaux",
+      "x": 4524.0,
+      "y": 9549.0
+    },
+    {
+      "name": "Le Moule",
+      "x": 2590.0,
+      "y": 5384.0
+    },
+    {
+      "name": "Regina",
+      "x": 7197.0,
+      "y": 2347.0
+    },
+    {
+      "name": "Durras",
+      "x": 8835.0,
+      "y": 2710.0
+    },
+    {
+      "name": "Saint Phillippe",
+      "x": 4674.0,
+      "y": 10730.0
+    },
+    {
+      "name": "Vernon",
+      "x": 9142.0,
+      "y": 2159.0
+    },
+    {
+      "name": "Lamentin",
+      "x": 1333.0,
+      "y": 5962.0
+    },
+    {
+      "name": "Saint Pierre",
+      "x": 9662.0,
+      "y": 1605.0
+    }
+  ]
+};
