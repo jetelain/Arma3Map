da4b3cd9cdd1058b8c19bd22b367efc57770e9c1
Karol <lokarcqaz@gmail.com>
2021-02-22 04:08:26 +0100

    sangin_distirict_helmand_province

---
+++ maps/sangin_distirict_helmand_province.js
@@ -0,0 +1,20 @@
+Arma3Map.Maps.sangin_distirict_helmand_province = {
+  CRS: MGRS_CRS(0.063, 0.063, 323),
+  "worldName": "sangin_distirict_helmand_province",
+  "tilePattern": "/maps/sangin_distirict_helmand_province/{z}/{x}/{y}.png",
+  "maxZoom": 3,
+  "minZoom": 0,
+  "defaultZoom": 2,
+  "attribution": "&copy; Bohemia Interactive",
+  "tileSize": 323,
+  "center": [
+    2560,
+    2560
+  ],
+  "worldSize": 5120.0,
+  "preview": null,
+  "dlc": null,
+  "title": "Sangin Distirict Helmand Province",
+  "steamWorkshop": null,
+  "cities": []
+};
