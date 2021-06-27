61db84626508526bfaf45040653c0014719e413e
Julien <julien.etelain@outlook.com>
2021-06-27 15:37:56 +0200

    Chernarus 2020 (featuring HiRes)

---
+++ maps/cup_chernarus_a3.js
@@ -0,0 +1,291 @@
+Arma3Map.Maps.cup_chernarus_a3 = {
+  CRS: MGRS_CRS(0.01575, 0.01575, 242),
+  "worldName": "cup_chernarus_a3",
+  "tilePattern": "/maps/cup_chernarus_a3/{z}/{x}/{y}.png",
+  "maxZoom": 6,
+  "minZoom": 0,
+  "defaultZoom": 2,
+  "attribution": "&copy; Bohemia Interactive, CUP Team",
+  "tileSize": 242,
+  "center": [
+    7680,
+    7680
+  ],
+  "worldSize": 15360.0,
+  "preview": "https://steamuserimages-a.akamaihd.net/ugc/773992385898088811/795C9083B9294539BA332216F514E64223005BDA/",
+  "dlc": null,
+  "title": "Chernarus 2020",
+  "steamWorkshop": "https://steamcommunity.com/sharedfiles/filedetails/?id=1981964169",
+  "cities": [
+    {
+      "name": "Novy Sobor",
+      "x": 7123.63,
+      "y": 7774.63
+    },
+    {
+      "name": "Stary Sobor",
+      "x": 6169.01,
+      "y": 7793.67
+    },
+    {
+      "name": "Guglovo",
+      "x": 8431.08,
+      "y": 6703.19
+    },
+    {
+      "name": "Vishnoye",
+      "x": 6561.91,
+      "y": 6078.77
+    },
+    {
+      "name": "Kabanino",
+      "x": 5300.71,
+      "y": 8645.9
+    },
+    {
+      "name": "Rogovo",
+      "x": 4770.96,
+      "y": 6835.26
+    },
+    {
+      "name": "Mogilevka",
+      "x": 7650.41,
+      "y": 5096.46
+    },
+    {
+      "name": "Gorka",
+      "x": 9616.06,
+      "y": 8914.38
+    },
+    {
+      "name": "Grishino",
+      "x": 5959.94,
+      "y": 10335.7
+    },
+    {
+      "name": "Shakhovka",
+      "x": 9728.64,
+      "y": 6567.3
+    },
+    {
+      "name": "Pogorevka",
+      "x": 4435.36,
+      "y": 6425.58
+    },
+    {
+      "name": "Pulkovo",
+      "x": 4950.82,
+      "y": 5627.1
+    },
+    {
+      "name": "Nadezhdino",
+      "x": 5876.66,
+      "y": 4696.93
+    },
+    {
+      "name": "Vybor",
+      "x": 3833.17,
+      "y": 8918.92
+    },
+    {
+      "name": "Polana",
+      "x": 10707.1,
+      "y": 8053.29
+    },
+    {
+      "name": "Staroye",
+      "x": 10212.5,
+      "y": 5385.04
+    },
+    {
+      "name": "Dubrovka",
+      "x": 10382.1,
+      "y": 9938.35
+    },
+    {
+      "name": "Pustoshka",
+      "x": 3070.23,
+      "y": 7940.48
+    },
+    {
+      "name": "Kozlovka",
+      "x": 4477.57,
+      "y": 4597.79
+    },
+    {
+      "name": "Pusta",
+      "x": 9127.18,
+      "y": 3954.47
+    },
+    {
+      "name": "Dolina",
+      "x": 11285.2,
+      "y": 6633.58
+    },
+    {
+      "name": "Gvozdno",
+      "x": 8614.62,
+      "y": 11890.3
+    },
+    {
+      "name": "Prigorodki",
+      "x": 8015.58,
+      "y": 3419.26
+    },
+    {
+      "name": "Drozhino",
+      "x": 3382.02,
+      "y": 4925.88
+    },
+    {
+      "name": "Sosnovka",
+      "x": 2539.26,
+      "y": 6358.71
+    },
+    {
+      "name": "Msta",
+      "x": 11333.8,
+      "y": 5421.4
+    },
+    {
+      "name": "Lopatino",
+      "x": 2750.0,
+      "y": 10005.0
+    },
+    {
+      "name": "Zelenogorsk",
+      "x": 2751.09,
+      "y": 5424.78
+    },
+    {
+      "name": "Orlovets",
+      "x": 12131.0,
+      "y": 7189.79
+    },
+    {
+      "name": "Berezino",
+      "x": 12014.0,
+      "y": 9090.84
+    },
+    {
+      "name": "Myshkino",
+      "x": 1998.18,
+      "y": 7355.13
+    },
+    {
+      "name": "Tchernogorsk",
+      "x": 6731.21,
+      "y": 2554.13
+    },
+    {
+      "name": "Zaprudnoe",
+      "x": 5293.84,
+      "y": 12715.7
+    },
+    {
+      "name": "Bor",
+      "x": 3326.74,
+      "y": 3886.73
+    },
+    {
+      "name": "Bogtyrka",
+      "x": 1601.56,
+      "y": 8971.45
+    },
+    {
+      "name": "Nizhnoye",
+      "x": 12824.9,
+      "y": 8097.7
+    },
+    {
+      "name": "Vavilovo",
+      "x": 2339.08,
+      "y": 11014.5
+    },
+    {
+      "name": "Balota",
+      "x": 4527.58,
+      "y": 2465.32
+    },
+    {
+      "name": "Khelm",
+      "x": 12329.3,
+      "y": 10771.0
+    },
+    {
+      "name": "Krasnostav",
+      "x": 11224.3,
+      "y": 12274.5
+    },
+    {
+      "name": "Topolniki",
+      "x": 2913.75,
+      "y": 12366.4
+    },
+    {
+      "name": "Komarovo",
+      "x": 3664.31,
+      "y": 2499.0
+    },
+    {
+      "name": "Novaya Petrovka",
+      "x": 3477.17,
+      "y": 12931.2
+    },
+    {
+      "name": "Zabolotye",
+      "x": 1171.34,
+      "y": 10033.0
+    },
+    {
+      "name": "Elektrozavodsk",
+      "x": 10313.7,
+      "y": 2159.41
+    },
+    {
+      "name": "Solnitchniy",
+      "x": 13397.4,
+      "y": 6336.84
+    },
+    {
+      "name": "Kamyshovo",
+      "x": 12063.1,
+      "y": 3515.58
+    },
+    {
+      "name": "Tulga",
+      "x": 12802.5,
+      "y": 4369.49
+    },
+    {
+      "name": "Pavlovo",
+      "x": 1693.98,
+      "y": 3858.65
+    },
+    {
+      "name": "Sinystok",
+      "x": 1538.28,
+      "y": 11904.6
+    },
+    {
+      "name": "Zvir",
+      "x": 489.97,
+      "y": 5222.08
+    },
+    {
+      "name": "Kamenka",
+      "x": 1853.09,
+      "y": 2249.85
+    },
+    {
+      "name": "Tisy",
+      "x": 3441.65,
+      "y": 14799.5
+    },
+    {
+      "name": "Olsha",
+      "x": 13331.0,
+      "y": 12900.0
+    }
+  ]
+};
