df607ae9b8e05b701066ef53393514d227369158
Julien <julien.etelain@outlook.com>
2021-11-07 23:53:21 +0100

    Update index

--- maps/all.js
+++ maps/all.js
@@ -507,6 +507,537 @@ Arma3Map.Maps.beketov = {
     }
   ]
 };
+Arma3Map.Maps.cam_lao_nam = {
+  CRS: MGRS_CRS(0.01575, 0.01575, 323),
+  "worldName": "cam_lao_nam",
+  "tilePattern": "/maps/cam_lao_nam/{z}/{x}/{y}.png",
+  "maxZoom": 6,
+  "minZoom": 0,
+  "defaultZoom": 2,
+  "attribution": "&copy; Bohemia Interactive",
+  "tileSize": 323,
+  "center": [
+    10240,
+    10240
+  ],
+  "worldSize": 20480.0,
+  "preview": null,
+  "dlc": null,
+  "title": "Cam Lao Nam",
+  "steamWorkshop": null,
+  "cities": [
+    {
+      "name": "Da Krong",
+      "x": 9600.51,
+      "y": 11414.3
+    },
+    {
+      "name": "Seg Begat",
+      "x": 11441.6,
+      "y": 9595.66
+    },
+    {
+      "name": "Nuoc Bun",
+      "x": 9102.26,
+      "y": 9190.4
+    },
+    {
+      "name": "Che",
+      "x": 8784.59,
+      "y": 10984.2
+    },
+    {
+      "name": "",
+      "x": 9282.1,
+      "y": 8782.22
+    },
+    {
+      "name": "La Hot",
+      "x": 9642.92,
+      "y": 12076.5
+    },
+    {
+      "name": "Niem Tin",
+      "x": 10517.1,
+      "y": 8258.42
+    },
+    {
+      "name": "A Lieng",
+      "x": 10696.4,
+      "y": 12389.5
+    },
+    {
+      "name": "Banh Trung",
+      "x": 12330.6,
+      "y": 11429.0
+    },
+    {
+      "name": "Tan Di",
+      "x": 9168.04,
+      "y": 12458.7
+    },
+    {
+      "name": "Lumphat",
+      "x": 7792.41,
+      "y": 9091.01
+    },
+    {
+      "name": "Attapeu",
+      "x": 7647.79,
+      "y": 11755.8
+    },
+    {
+      "name": "Kiem Tra",
+      "x": 9722.25,
+      "y": 7148.83
+    },
+    {
+      "name": "West Hanoi",
+      "x": 11862.1,
+      "y": 13099.4
+    },
+    {
+      "name": "Gan Song",
+      "x": 11655.9,
+      "y": 6873.52
+    },
+    {
+      "name": "Bru",
+      "x": 8701.86,
+      "y": 13768.0
+    },
+    {
+      "name": "Yen Tinh",
+      "x": 6441.94,
+      "y": 9210.99
+    },
+    {
+      "name": "Hanoi",
+      "x": 13105.1,
+      "y": 13211.2
+    },
+    {
+      "name": "Long Hai",
+      "x": 8750.48,
+      "y": 5599.88
+    },
+    {
+      "name": "Lang Mau",
+      "x": 15093.9,
+      "y": 11111.5
+    },
+    {
+      "name": "North Hanoi",
+      "x": 13589.5,
+      "y": 13994.7
+    },
+    {
+      "name": "Thung Lung Cao",
+      "x": 5127.02,
+      "y": 9461.74
+    },
+    {
+      "name": "Rung Cung",
+      "x": 7258.06,
+      "y": 14914.2
+    },
+    {
+      "name": "Giua Ho",
+      "x": 6029.36,
+      "y": 13896.3
+    },
+    {
+      "name": "Hon Ba",
+      "x": 10371.7,
+      "y": 4633.85
+    },
+    {
+      "name": "Hai Phong",
+      "x": 15128.3,
+      "y": 13134.7
+    },
+    {
+      "name": "Mien",
+      "x": 8298.5,
+      "y": 4798.24
+    },
+    {
+      "name": "Bo Song",
+      "x": 15968.6,
+      "y": 8828.27
+    },
+    {
+      "name": "Bach Dang",
+      "x": 13111.5,
+      "y": 16012.8
+    },
+    {
+      "name": "Tan Hop",
+      "x": 15890.1,
+      "y": 13421.9
+    },
+    {
+      "name": "Bok Raz Ocra",
+      "x": 12302.9,
+      "y": 16458.6
+    },
+    {
+      "name": "Dao Trai",
+      "x": 5817.91,
+      "y": 15127.3
+    },
+    {
+      "name": "Cu Chi",
+      "x": 15251.4,
+      "y": 5782.33
+    },
+    {
+      "name": "Can Tho",
+      "x": 13435.8,
+      "y": 4333.85
+    },
+    {
+      "name": "Bai Chai",
+      "x": 14122.2,
+      "y": 15809.2
+    },
+    {
+      "name": "Phao Noi",
+      "x": 11992.4,
+      "y": 16964.6
+    },
+    {
+      "name": "Sam Song",
+      "x": 17049.6,
+      "y": 11775.4
+    },
+    {
+      "name": "Ket Thuc",
+      "x": 7668.67,
+      "y": 16738.7
+    },
+    {
+      "name": "Sihanoukville",
+      "x": 3314.86,
+      "y": 8430.22
+    },
+    {
+      "name": "Hue",
+      "x": 17344.2,
+      "y": 9065.26
+    },
+    {
+      "name": "Nong Khiaw",
+      "x": 4788.55,
+      "y": 15078.0
+    },
+    {
+      "name": "Ban Len",
+      "x": 3214.79,
+      "y": 12773.1
+    },
+    {
+      "name": "Nhen Den",
+      "x": 13612.8,
+      "y": 3574.16
+    },
+    {
+      "name": "Binh Yen",
+      "x": 2822.13,
+      "y": 11769.9
+    },
+    {
+      "name": "Muy Lai",
+      "x": 17805.0,
+      "y": 10718.3
+    },
+    {
+      "name": "Nga Tu",
+      "x": 2621.5,
+      "y": 11246.4
+    },
+    {
+      "name": "Pak Noi",
+      "x": 10464.2,
+      "y": 18110.6
+    },
+    {
+      "name": "Na Lai",
+      "x": 18180.8,
+      "y": 10031.1
+    },
+    {
+      "name": "Ca Can",
+      "x": 18284.5,
+      "y": 10814.8
+    },
+    {
+      "name": "Vong Xo",
+      "x": 5762.95,
+      "y": 17035.1
+    },
+    {
+      "name": "Man Tau",
+      "x": 14593.1,
+      "y": 17155.5
+    },
+    {
+      "name": "Cay Mo",
+      "x": 18643.9,
+      "y": 10568.3
+    },
+    {
+      "name": "Ban Dao",
+      "x": 14396.0,
+      "y": 17645.6
+    },
+    {
+      "name": "Lho Berro Ghi",
+      "x": 18695.5,
+      "y": 9250.16
+    },
+    {
+      "name": "Xo Xanh",
+      "x": 1711.41,
+      "y": 10413.4
+    },
+    {
+      "name": "Buio Sang",
+      "x": 18199.8,
+      "y": 7160.05
+    },
+    {
+      "name": "Diem Dang",
+      "x": 14191.6,
+      "y": 17828.5
+    },
+    {
+      "name": "Phok Ham",
+      "x": 2227.32,
+      "y": 13393.3
+    },
+    {
+      "name": "Do Con",
+      "x": 18851.3,
+      "y": 10249.5
+    },
+    {
+      "name": "Bai Bien Dep",
+      "x": 1497.58,
+      "y": 9456.38
+    },
+    {
+      "name": "Xom Ram",
+      "x": 14243.9,
+      "y": 18076.9
+    },
+    {
+      "name": "Saigon",
+      "x": 16324.6,
+      "y": 3725.15
+    },
+    {
+      "name": "Xom Gia",
+      "x": 14119.8,
+      "y": 18469.9
+    },
+    {
+      "name": "Thu Phac",
+      "x": 13764.6,
+      "y": 18674.7
+    },
+    {
+      "name": "Tong Moo",
+      "x": 13392.5,
+      "y": 18824.8
+    },
+    {
+      "name": "Cong Troi",
+      "x": 1087.91,
+      "y": 9879.05
+    },
+    {
+      "name": "Cau Hai",
+      "x": 19184.5,
+      "y": 7980.36
+    },
+    {
+      "name": "Phu An",
+      "x": 19352.0,
+      "y": 8708.08
+    },
+    {
+      "name": "Tau Phabang",
+      "x": 15943.9,
+      "y": 17513.6
+    },
+    {
+      "name": "Bao Ve",
+      "x": 1026.27,
+      "y": 9392.84
+    },
+    {
+      "name": "Kro Sang",
+      "x": 1051.73,
+      "y": 11344.4
+    },
+    {
+      "name": "Tieng Tai",
+      "x": 12430.3,
+      "y": 19327.7
+    },
+    {
+      "name": "Pho Duc",
+      "x": 984.55,
+      "y": 8860.33
+    },
+    {
+      "name": "Mekong Delta",
+      "x": 15332.6,
+      "y": 2267.45
+    },
+    {
+      "name": "Hoa Lien",
+      "x": 19242.7,
+      "y": 7250.24
+    },
+    {
+      "name": "Ca Loi",
+      "x": 13062.8,
+      "y": 19313.6
+    },
+    {
+      "name": "Bolar Gier Hot",
+      "x": 19151.8,
+      "y": 13548.5
+    },
+    {
+      "name": "Soc Trang",
+      "x": 14507.6,
+      "y": 1690.65
+    },
+    {
+      "name": "Paradise Island",
+      "x": 19860.6,
+      "y": 9722.26
+    },
+    {
+      "name": "Com Ngon",
+      "x": 1238.05,
+      "y": 13706.2
+    },
+    {
+      "name": "Ba Ria II",
+      "x": 18401.7,
+      "y": 5037.61
+    },
+    {
+      "name": "Don Lac",
+      "x": 13263.4,
+      "y": 19440.0
+    },
+    {
+      "name": "Dong Xa",
+      "x": 15888.4,
+      "y": 18255.0
+    },
+    {
+      "name": "Ca Ngon",
+      "x": 12690.3,
+      "y": 19757.8
+    },
+    {
+      "name": "Ba Ria",
+      "x": 18036.2,
+      "y": 4178.44
+    },
+    {
+      "name": "Sang Ha",
+      "x": 1877.16,
+      "y": 15524.1
+    },
+    {
+      "name": "Sai Port",
+      "x": 16942.1,
+      "y": 2937.68
+    },
+    {
+      "name": "Quyen",
+      "x": 13091.2,
+      "y": 19763.4
+    },
+    {
+      "name": "Tam Pep",
+      "x": 16158.1,
+      "y": 2227.75
+    },
+    {
+      "name": "Da Nang",
+      "x": 19619.7,
+      "y": 6796.9
+    },
+    {
+      "name": "Va Cang",
+      "x": 2707.14,
+      "y": 16919.0
+    },
+    {
+      "name": "Binh Minh",
+      "x": 19346.9,
+      "y": 5671.66
+    },
+    {
+      "name": "An Hoa",
+      "x": 19193.5,
+      "y": 5353.64
+    },
+    {
+      "name": "Bam Bon",
+      "x": 1089.5,
+      "y": 15144.2
+    },
+    {
+      "name": "Dharma Docks",
+      "x": 2558.5,
+      "y": 2971.78
+    },
+    {
+      "name": "Marble Mtn.",
+      "x": 20168.1,
+      "y": 6251.46
+    },
+    {
+      "name": "Kenglat",
+      "x": 2338.94,
+      "y": 17525.9
+    },
+    {
+      "name": "Lac My",
+      "x": 19451.7,
+      "y": 4582.99
+    },
+    {
+      "name": "An Sung Song",
+      "x": 1610.54,
+      "y": 16754.7
+    },
+    {
+      "name": "Pat Mep",
+      "x": 19044.2,
+      "y": 3941.94
+    },
+    {
+      "name": "Lai Chau",
+      "x": 3957.71,
+      "y": 19613.5
+    },
+    {
+      "name": "Nodal Lho",
+      "x": 1605.09,
+      "y": 19185.5
+    }
+  ]
+};
 Arma3Map.Maps.chernarus = {
   CRS: MGRS_CRS(0.01575, 0.01575, 242),
   "worldName": "chernarus",
@@ -2816,6 +3347,322 @@ Arma3Map.Maps.fata = {
       "name": "Bahad",
       "x": 4330.89,
       "y": 6985.35
+    }
+  ]
+};
+Arma3Map.Maps.gulfcoast = {
+  CRS: MGRS_CRS(0.01575, 0.01575, 242),
+  "worldName": "gulfcoast",
+  "tilePattern": "/maps/gulfcoast/{z}/{x}/{y}.png",
+  "maxZoom": 6,
+  "minZoom": 0,
+  "defaultZoom": 2,
+  "attribution": "&copy; Bohemia Interactive",
+  "tileSize": 242,
+  "center": [
+    7680,
+    7680
+  ],
+  "worldSize": 15360.0,
+  "preview": null,
+  "dlc": null,
+  "title": "Gulfcoast",
+  "steamWorkshop": null,
+  "cities": [
+    {
+      "name": "Orion Industrial",
+      "x": 9857.7,
+      "y": 7884.98
+    },
+    {
+      "name": "Port Orion",
+      "x": 9032.63,
+      "y": 7363.43
+    },
+    {
+      "name": "Alva",
+      "x": 8039.45,
+      "y": 7971.2
+    },
+    {
+      "name": "Orion Estates",
+      "x": 9116.62,
+      "y": 7105.37
+    },
+    {
+      "name": "Eastwoods",
+      "x": 9630.25,
+      "y": 10045.0
+    },
+    {
+      "name": "Arcadia",
+      "x": 10411.4,
+      "y": 5922.39
+    },
+    {
+      "name": "LaBelle",
+      "x": 6341.57,
+      "y": 8822.01
+    },
+    {
+      "name": "Cross City",
+      "x": 10517.7,
+      "y": 10935.9
+    },
+    {
+      "name": "Palm Dale",
+      "x": 6317.56,
+      "y": 6822.5
+    },
+    {
+      "name": "Harker",
+      "x": 8189.51,
+      "y": 5101.96
+    },
+    {
+      "name": "North Fort",
+      "x": 12070.2,
+      "y": 9608.02
+    },
+    {
+      "name": "Sebring",
+      "x": 8023.88,
+      "y": 11333.8
+    },
+    {
+      "name": "Pine Ridge",
+      "x": 5794.0,
+      "y": 7606.68
+    },
+    {
+      "name": "Cape Rassa",
+      "x": 7019.92,
+      "y": 5343.96
+    },
+    {
+      "name": "Goodland",
+      "x": 5735.03,
+      "y": 10106.2
+    },
+    {
+      "name": "Keela",
+      "x": 8104.41,
+      "y": 4165.12
+    },
+    {
+      "name": "Iona City",
+      "x": 7414.71,
+      "y": 4097.08
+    },
+    {
+      "name": "Estero Pass",
+      "x": 6478.0,
+      "y": 4358.27
+    },
+    {
+      "name": "Lake Port",
+      "x": 10621.2,
+      "y": 12606.8
+    },
+    {
+      "name": "Copperhead",
+      "x": 11269.4,
+      "y": 12532.4
+    },
+    {
+      "name": "Lely",
+      "x": 5782.69,
+      "y": 11838.4
+    },
+    {
+      "name": "Bella Vida",
+      "x": 4300.22,
+      "y": 6930.67
+    },
+    {
+      "name": "Tice",
+      "x": 12442.8,
+      "y": 4444.06
+    },
+    {
+      "name": "Felda",
+      "x": 11835.7,
+      "y": 12423.3
+    },
+    {
+      "name": "Hobe Sound",
+      "x": 13238.7,
+      "y": 5318.21
+    },
+    {
+      "name": "Captiva Beach",
+      "x": 7368.86,
+      "y": 3183.74
+    },
+    {
+      "name": "East Point Port",
+      "x": 14379.2,
+      "y": 8796.63
+    },
+    {
+      "name": "Solana",
+      "x": 14091.3,
+      "y": 10037.6
+    },
+    {
+      "name": "St James City",
+      "x": 3758.53,
+      "y": 8770.34
+    },
+    {
+      "name": "Edgewood",
+      "x": 14656.0,
+      "y": 7292.88
+    },
+    {
+      "name": "Haven",
+      "x": 10996.5,
+      "y": 2797.21
+    },
+    {
+      "name": "Croom",
+      "x": 14112.6,
+      "y": 11054.4
+    },
+    {
+      "name": "Hurricane Pass",
+      "x": 4936.63,
+      "y": 4150.95
+    },
+    {
+      "name": "Rotonda",
+      "x": 11030.4,
+      "y": 13877.3
+    },
+    {
+      "name": "Osprey",
+      "x": 9965.53,
+      "y": 14324.0
+    },
+    {
+      "name": "Bokeelia",
+      "x": 12487.8,
+      "y": 13409.8
+    },
+    {
+      "name": "Captiva Sound",
+      "x": 8368.7,
+      "y": 1980.39
+    },
+    {
+      "name": "Sanibel",
+      "x": 5554.27,
+      "y": 2914.88
+    },
+    {
+      "name": "St Armand",
+      "x": 14808.8,
+      "y": 5367.9
+    },
+    {
+      "name": "Riverside",
+      "x": 14358.4,
+      "y": 4174.75
+    },
+    {
+      "name": "Lochmoor",
+      "x": 2915.3,
+      "y": 5778.66
+    },
+    {
+      "name": "Harborside",
+      "x": 14286.5,
+      "y": 12571.3
+    },
+    {
+      "name": "Sanibel Shores",
+      "x": 4088.0,
+      "y": 3507.96
+    },
+    {
+      "name": "Salvista",
+      "x": 2149.04,
+      "y": 8719.14
+    },
+    {
+      "name": "Laurel",
+      "x": 4929.61,
+      "y": 13966.7
+    },
+    {
+      "name": "Boca Grande",
+      "x": 2017.87,
+      "y": 10219.9
+    },
+    {
+      "name": "Del Mare",
+      "x": 1993.84,
+      "y": 5189.29
+    },
+    {
+      "name": "Woodford",
+      "x": 14753.9,
+      "y": 2692.72
+    },
+    {
+      "name": "Gulfshore",
+      "x": 13042.7,
+      "y": 1045.27
+    },
+    {
+      "name": "Corel Springs",
+      "x": 14418.7,
+      "y": 14632.7
+    },
+    {
+      "name": "Cayo Costa",
+      "x": 14645.8,
+      "y": 1956.13
+    },
+    {
+      "name": "Marco",
+      "x": 3104.94,
+      "y": 14151.1
+    },
+    {
+      "name": "Terra Ceia",
+      "x": 1940.31,
+      "y": 12772.3
+    },
+    {
+      "name": "Acline",
+      "x": 14112.3,
+      "y": 1028.85
+    },
+    {
+      "name": "North Terra",
+      "x": 1819.3,
+      "y": 13449.7
+    },
+    {
+      "name": "Madeira",
+      "x": 13990.2,
+      "y": 480.99
+    },
+    {
+      "name": "Herrons Landing",
+      "x": 2149.94,
+      "y": 2255.12
+    },
+    {
+      "name": "SanCarlos",
+      "x": 726.84,
+      "y": 14388.3
+    },
+    {
+      "name": "Gasparilla Sound",
+      "x": 815.16,
+      "y": 892.99
     }
   ]
 };
