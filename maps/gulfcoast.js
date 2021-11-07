cb464d65df0a6f933fb0a1fb87f85ca9927cfb14
RandomUsernameThatIsNotYetTaken <45180330+RandomUsernameThatIsNotYetTaken@users.noreply.github.com>
2021-11-07 23:45:32 +0100

    Added GulfCoast Islands and PF CDLC Cam Lao Nam map (#16)

---
+++ maps/gulfcoast.js
@@ -0,0 +1,316 @@
+Arma3Map.Maps.gulfcoast = {
+  CRS: MGRS_CRS(0.01575, 0.01575, 242),
+  "worldName": "gulfcoast",
+  "tilePattern": "/maps/gulfcoast/{z}/{x}/{y}.png",
+  "maxZoom": 6,
+  "minZoom": 0,
+  "defaultZoom": 2,
+  "attribution": "&copy; Bohemia Interactive",
+  "tileSize": 242,
+  "center": [
+    7680,
+    7680
+  ],
+  "worldSize": 15360.0,
+  "preview": null,
+  "dlc": null,
+  "title": "Gulfcoast",
+  "steamWorkshop": null,
+  "cities": [
+    {
+      "name": "Orion Industrial",
+      "x": 9857.7,
+      "y": 7884.98
+    },
+    {
+      "name": "Port Orion",
+      "x": 9032.63,
+      "y": 7363.43
+    },
+    {
+      "name": "Alva",
+      "x": 8039.45,
+      "y": 7971.2
+    },
+    {
+      "name": "Orion Estates",
+      "x": 9116.62,
+      "y": 7105.37
+    },
+    {
+      "name": "Eastwoods",
+      "x": 9630.25,
+      "y": 10045.0
+    },
+    {
+      "name": "Arcadia",
+      "x": 10411.4,
+      "y": 5922.39
+    },
+    {
+      "name": "LaBelle",
+      "x": 6341.57,
+      "y": 8822.01
+    },
+    {
+      "name": "Cross City",
+      "x": 10517.7,
+      "y": 10935.9
+    },
+    {
+      "name": "Palm Dale",
+      "x": 6317.56,
+      "y": 6822.5
+    },
+    {
+      "name": "Harker",
+      "x": 8189.51,
+      "y": 5101.96
+    },
+    {
+      "name": "North Fort",
+      "x": 12070.2,
+      "y": 9608.02
+    },
+    {
+      "name": "Sebring",
+      "x": 8023.88,
+      "y": 11333.8
+    },
+    {
+      "name": "Pine Ridge",
+      "x": 5794.0,
+      "y": 7606.68
+    },
+    {
+      "name": "Cape Rassa",
+      "x": 7019.92,
+      "y": 5343.96
+    },
+    {
+      "name": "Goodland",
+      "x": 5735.03,
+      "y": 10106.2
+    },
+    {
+      "name": "Keela",
+      "x": 8104.41,
+      "y": 4165.12
+    },
+    {
+      "name": "Iona City",
+      "x": 7414.71,
+      "y": 4097.08
+    },
+    {
+      "name": "Estero Pass",
+      "x": 6478.0,
+      "y": 4358.27
+    },
+    {
+      "name": "Lake Port",
+      "x": 10621.2,
+      "y": 12606.8
+    },
+    {
+      "name": "Copperhead",
+      "x": 11269.4,
+      "y": 12532.4
+    },
+    {
+      "name": "Lely",
+      "x": 5782.69,
+      "y": 11838.4
+    },
+    {
+      "name": "Bella Vida",
+      "x": 4300.22,
+      "y": 6930.67
+    },
+    {
+      "name": "Tice",
+      "x": 12442.8,
+      "y": 4444.06
+    },
+    {
+      "name": "Felda",
+      "x": 11835.7,
+      "y": 12423.3
+    },
+    {
+      "name": "Hobe Sound",
+      "x": 13238.7,
+      "y": 5318.21
+    },
+    {
+      "name": "Captiva Beach",
+      "x": 7368.86,
+      "y": 3183.74
+    },
+    {
+      "name": "East Point Port",
+      "x": 14379.2,
+      "y": 8796.63
+    },
+    {
+      "name": "Solana",
+      "x": 14091.3,
+      "y": 10037.6
+    },
+    {
+      "name": "St James City",
+      "x": 3758.53,
+      "y": 8770.34
+    },
+    {
+      "name": "Edgewood",
+      "x": 14656.0,
+      "y": 7292.88
+    },
+    {
+      "name": "Haven",
+      "x": 10996.5,
+      "y": 2797.21
+    },
+    {
+      "name": "Croom",
+      "x": 14112.6,
+      "y": 11054.4
+    },
+    {
+      "name": "Hurricane Pass",
+      "x": 4936.63,
+      "y": 4150.95
+    },
+    {
+      "name": "Rotonda",
+      "x": 11030.4,
+      "y": 13877.3
+    },
+    {
+      "name": "Osprey",
+      "x": 9965.53,
+      "y": 14324.0
+    },
+    {
+      "name": "Bokeelia",
+      "x": 12487.8,
+      "y": 13409.8
+    },
+    {
+      "name": "Captiva Sound",
+      "x": 8368.7,
+      "y": 1980.39
+    },
+    {
+      "name": "Sanibel",
+      "x": 5554.27,
+      "y": 2914.88
+    },
+    {
+      "name": "St Armand",
+      "x": 14808.8,
+      "y": 5367.9
+    },
+    {
+      "name": "Riverside",
+      "x": 14358.4,
+      "y": 4174.75
+    },
+    {
+      "name": "Lochmoor",
+      "x": 2915.3,
+      "y": 5778.66
+    },
+    {
+      "name": "Harborside",
+      "x": 14286.5,
+      "y": 12571.3
+    },
+    {
+      "name": "Sanibel Shores",
+      "x": 4088.0,
+      "y": 3507.96
+    },
+    {
+      "name": "Salvista",
+      "x": 2149.04,
+      "y": 8719.14
+    },
+    {
+      "name": "Laurel",
+      "x": 4929.61,
+      "y": 13966.7
+    },
+    {
+      "name": "Boca Grande",
+      "x": 2017.87,
+      "y": 10219.9
+    },
+    {
+      "name": "Del Mare",
+      "x": 1993.84,
+      "y": 5189.29
+    },
+    {
+      "name": "Woodford",
+      "x": 14753.9,
+      "y": 2692.72
+    },
+    {
+      "name": "Gulfshore",
+      "x": 13042.7,
+      "y": 1045.27
+    },
+    {
+      "name": "Corel Springs",
+      "x": 14418.7,
+      "y": 14632.7
+    },
+    {
+      "name": "Cayo Costa",
+      "x": 14645.8,
+      "y": 1956.13
+    },
+    {
+      "name": "Marco",
+      "x": 3104.94,
+      "y": 14151.1
+    },
+    {
+      "name": "Terra Ceia",
+      "x": 1940.31,
+      "y": 12772.3
+    },
+    {
+      "name": "Acline",
+      "x": 14112.3,
+      "y": 1028.85
+    },
+    {
+      "name": "North Terra",
+      "x": 1819.3,
+      "y": 13449.7
+    },
+    {
+      "name": "Madeira",
+      "x": 13990.2,
+      "y": 480.99
+    },
+    {
+      "name": "Herrons Landing",
+      "x": 2149.94,
+      "y": 2255.12
+    },
+    {
+      "name": "SanCarlos",
+      "x": 726.84,
+      "y": 14388.3
+    },
+    {
+      "name": "Gasparilla Sound",
+      "x": 815.16,
+      "y": 892.99
+    }
+  ]
+};
