780e17959ace93a0b6e538ba703161eb171b79d1
ChocolateTilt <dmhansen65@gmail.com>
2022-02-01 23:26:42 -0500

    Add Scottish Highlands

---
+++ maps/oski_corran.js
@@ -0,0 +1,46 @@
+Arma3Map.Maps.oski_corran = {
+  CRS: MGRS_CRS(0.0234375, 0.0234375, 241),
+  "worldName": "oski_corran",
+  "tilePattern": "/maps/oski_corran/{z}/{x}/{y}.png",
+  "maxZoom": 5,
+  "minZoom": 0,
+  "defaultZoom": 2,
+  "attribution": "&copy; Bohemia Interactive",
+  "tileSize": 241,
+  "center": [
+    5120,
+    5120
+  ],
+  "worldSize": 10240.0,
+  "preview": null,
+  "dlc": null,
+  "title": "Scottish Highlands",
+  "steamWorkshop": null,
+  "cities": [
+    {
+      "name": "Clovullin",
+      "x": 4530.58,
+      "y": 3587.94
+    },
+    {
+      "name": "Onich",
+      "x": 6461.95,
+      "y": 1777.22
+    },
+    {
+      "name": "Bunree",
+      "x": 6301.59,
+      "y": 3164.35
+    },
+    {
+      "name": "Corran",
+      "x": 6328.3,
+      "y": 3810.98
+    },
+    {
+      "name": "North Ballachulish",
+      "x": 9475.56,
+      "y": 724.24
+    }
+  ]
+};
