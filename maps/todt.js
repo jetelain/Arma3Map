8912f0c3e8059ad23ebcdea495ac22f85ae8bd71
draniak <51991753+draniak@users.noreply.github.com>
2022-02-12 20:07:50 +0100

    TODT + TRIA (#23)

---
+++ maps/todt.js
@@ -0,0 +1,20 @@
+Arma3Map.Maps.todt = {
+  CRS: MGRS_CRS(0.02475, 0.02475, 254),
+  "worldName": "todt",
+  "tilePattern": "/maps/todt/{z}/{x}/{y}.png",
+  "maxZoom": 5,
+  "minZoom": 0,
+  "defaultZoom": 2,
+  "attribution": "&copy; Bohemia Interactive",
+  "tileSize": 254,
+  "center": [
+    5120,
+    5120
+  ],
+  "worldSize": 10240.0,
+  "preview": null,
+  "dlc": null,
+  "title": "Todt",
+  "steamWorkshop": null,
+  "cities": []
+};
