7b92b3fb0fee8aa5c41ed106907de1382f5c71ae
draniak <51991753+draniak@users.noreply.github.com>
2022-02-12 19:37:20 +0100

    Ajout de Tria

---
+++ maps/optre_phobos.js
@@ -0,0 +1,20 @@
+Arma3Map.Maps.optre_phobos = {
+  CRS: MGRS_CRS(0.012375, 0.012375, 203),
+  "worldName": "optre_phobos",
+  "tilePattern": "/maps/optre_phobos/{z}/{x}/{y}.png",
+  "maxZoom": 6,
+  "minZoom": 0,
+  "defaultZoom": 2,
+  "attribution": "&copy; Bohemia Interactive",
+  "tileSize": 203,
+  "center": [
+    8192,
+    8192
+  ],
+  "worldSize": 16384.0,
+  "preview": null,
+  "dlc": null,
+  "title": "Phobos",
+  "steamWorkshop": null,
+  "cities": []
+};
