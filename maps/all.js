c94b98cc76f4e8b4cc9b3f8b826a511e097dd1b0
Julien Etelain <julien.etelain@outlook.com>
2022-04-27 22:26:22 +0200

    Deniland #24

--- maps/all.js
+++ maps/all.js
@@ -2612,6 +2612,757 @@ Arma3Map.Maps.cup_chernarus_a3 = {
     }
   ]
 };
+Arma3Map.Maps.deniland = {
+  CRS: MGRS_CRS(0.007875, 0.007875, 323),
+  "worldName": "deniland",
+  "tilePattern": "/maps/deniland/{z}/{x}/{y}.png",
+  "maxZoom": 7,
+  "minZoom": 0,
+  "defaultZoom": 3,
+  "attribution": "&copy; Bohemia Interactive",
+  "tileSize": 323,
+  "center": [
+    20480,
+    20480
+  ],
+  "worldSize": 40960.0,
+  "preview": "https://steamuserimages-a.akamaihd.net/ugc/921414614513589629/6DAFA3A4A7CD40EFCFAF407943A32B5D8CAF8399/",
+  "dlc": null,
+  "title": "Deniland",
+  "steamWorkshop": "https://steamcommunity.com/sharedfiles/filedetails/?id=1231955722",
+  "cities": [
+    {
+      "name": "Višnjevac",
+      "x": 19015.2,
+      "y": 22346.7
+    },
+    {
+      "name": "Stevac",
+      "x": 22887.9,
+      "y": 21955.6
+    },
+    {
+      "name": "Trešnjić",
+      "x": 19712.6,
+      "y": 23455.7
+    },
+    {
+      "name": "Babine",
+      "x": 23129.6,
+      "y": 18884.6
+    },
+    {
+      "name": "Jabukovac",
+      "x": 16477.0,
+      "y": 21059.8
+    },
+    {
+      "name": "Velika bukva",
+      "x": 18588.8,
+      "y": 15285.9
+    },
+    {
+      "name": "Ratina",
+      "x": 20657.6,
+      "y": 26060.7
+    },
+    {
+      "name": "Novo Orlovište",
+      "x": 25267.1,
+      "y": 23374.2
+    },
+    {
+      "name": "Staro Orlovište",
+      "x": 24101.1,
+      "y": 24850.3
+    },
+    {
+      "name": "Dekivci",
+      "x": 14211.4,
+      "y": 18788.5
+    },
+    {
+      "name": "Jablanica",
+      "x": 22640.5,
+      "y": 27150.7
+    },
+    {
+      "name": "Kamenica",
+      "x": 20611.0,
+      "y": 27585.1
+    },
+    {
+      "name": "Nehruovci",
+      "x": 13496.8,
+      "y": 18984.8
+    },
+    {
+      "name": "Kamendol",
+      "x": 12862.9,
+      "y": 21533.4
+    },
+    {
+      "name": "Gornja Boranja",
+      "x": 14025.1,
+      "y": 24802.6
+    },
+    {
+      "name": "Donja Boranja",
+      "x": 13724.9,
+      "y": 24943.6
+    },
+    {
+      "name": "Kladanj",
+      "x": 18735.6,
+      "y": 12524.2
+    },
+    {
+      "name": "Topola",
+      "x": 26267.0,
+      "y": 26806.3
+    },
+    {
+      "name": "Fuxin",
+      "x": 28912.6,
+      "y": 18122.8
+    },
+    {
+      "name": "Sirogojno",
+      "x": 29380.3,
+      "y": 23374.9
+    },
+    {
+      "name": "Put oslobodjenja",
+      "x": 23512.7,
+      "y": 29625.0
+    },
+    {
+      "name": "Put oslobođenja",
+      "x": 23712.7,
+      "y": 29625.0
+    },
+    {
+      "name": "Bela bara",
+      "x": 11342.5,
+      "y": 17110.2
+    },
+    {
+      "name": "Vrapci",
+      "x": 11339.6,
+      "y": 24317.2
+    },
+    {
+      "name": "Čehin",
+      "x": 29562.6,
+      "y": 16401.7
+    },
+    {
+      "name": "Mala stena",
+      "x": 15683.0,
+      "y": 11502.7
+    },
+    {
+      "name": "Žuti kamen",
+      "x": 14891.5,
+      "y": 29196.8
+    },
+    {
+      "name": "Necići",
+      "x": 25607.4,
+      "y": 11365.8
+    },
+    {
+      "name": "Crna bara",
+      "x": 10506.7,
+      "y": 17234.2
+    },
+    {
+      "name": "Plana",
+      "x": 25364.0,
+      "y": 29812.1
+    },
+    {
+      "name": "Stari Koritnik",
+      "x": 9835.08,
+      "y": 19745.7
+    },
+    {
+      "name": "Veliki Mrav",
+      "x": 21996.1,
+      "y": 9911.58
+    },
+    {
+      "name": "Rudnjak",
+      "x": 18376.4,
+      "y": 30966.3
+    },
+    {
+      "name": "Bare",
+      "x": 24391.9,
+      "y": 30452.0
+    },
+    {
+      "name": "Mali Mrav",
+      "x": 23635.8,
+      "y": 10120.8
+    },
+    {
+      "name": "Vukovac gornji",
+      "x": 15023.7,
+      "y": 11025.1
+    },
+    {
+      "name": "Lordovac",
+      "x": 13397.2,
+      "y": 29006.6
+    },
+    {
+      "name": "Novi Koritnik",
+      "x": 9184.69,
+      "y": 19675.1
+    },
+    {
+      "name": "Vukovac donji",
+      "x": 14152.6,
+      "y": 11039.7
+    },
+    {
+      "name": "Novo selo",
+      "x": 30765.6,
+      "y": 15616.1
+    },
+    {
+      "name": "Velika stena",
+      "x": 15918.6,
+      "y": 9895.46
+    },
+    {
+      "name": "Vranovo",
+      "x": 29991.3,
+      "y": 27189.8
+    },
+    {
+      "name": "Vidikovac",
+      "x": 20895.3,
+      "y": 8630.18
+    },
+    {
+      "name": "Mandre",
+      "x": 32434.0,
+      "y": 18522.6
+    },
+    {
+      "name": "Magaš",
+      "x": 24897.2,
+      "y": 32117.3
+    },
+    {
+      "name": "Staro selo",
+      "x": 31856.9,
+      "y": 14603.3
+    },
+    {
+      "name": "Stenovac",
+      "x": 25307.2,
+      "y": 8482.55
+    },
+    {
+      "name": "Gornji Dolac",
+      "x": 26802.3,
+      "y": 31784.2
+    },
+    {
+      "name": "Novograd",
+      "x": 9243.98,
+      "y": 26988.9
+    },
+    {
+      "name": "Jevramovac",
+      "x": 7750.56,
+      "y": 17759.8
+    },
+    {
+      "name": "Donji Dolac",
+      "x": 26377.9,
+      "y": 32385.0
+    },
+    {
+      "name": "Mramor",
+      "x": 33745.6,
+      "y": 22602.3
+    },
+    {
+      "name": "Silni",
+      "x": 7653.42,
+      "y": 16406.5
+    },
+    {
+      "name": "Denivci",
+      "x": 23556.7,
+      "y": 7376.2
+    },
+    {
+      "name": "Mrkalji",
+      "x": 12358.6,
+      "y": 9728.56
+    },
+    {
+      "name": "Lukovo",
+      "x": 7805.63,
+      "y": 15831.6
+    },
+    {
+      "name": "Rudnica",
+      "x": 11325.9,
+      "y": 30669.2
+    },
+    {
+      "name": "Visoko polje",
+      "x": 8528.03,
+      "y": 13638.2
+    },
+    {
+      "name": "Biberovac",
+      "x": 18071.8,
+      "y": 34077.8
+    },
+    {
+      "name": "Dvorane",
+      "x": 30884.3,
+      "y": 11379.2
+    },
+    {
+      "name": "Lece",
+      "x": 21812.6,
+      "y": 34253.6
+    },
+    {
+      "name": "Ivanje",
+      "x": 23713.4,
+      "y": 34028.5
+    },
+    {
+      "name": "Dencity",
+      "x": 34586.8,
+      "y": 20708.3
+    },
+    {
+      "name": "Markograd",
+      "x": 6305.57,
+      "y": 21072.5
+    },
+    {
+      "name": "Titograd",
+      "x": 6307.6,
+      "y": 19691.2
+    },
+    {
+      "name": "Gradac",
+      "x": 14789.2,
+      "y": 33548.9
+    },
+    {
+      "name": "Stari Brest",
+      "x": 33069.8,
+      "y": 27331.2
+    },
+    {
+      "name": "Kram",
+      "x": 12300.9,
+      "y": 8535.41
+    },
+    {
+      "name": "Spavčići",
+      "x": 6678.69,
+      "y": 16053.1
+    },
+    {
+      "name": "Vulešin",
+      "x": 13197.6,
+      "y": 7914.15
+    },
+    {
+      "name": "Crno polje",
+      "x": 20923.3,
+      "y": 5910.13
+    },
+    {
+      "name": "Drvar",
+      "x": 18567.1,
+      "y": 34948.3
+    },
+    {
+      "name": "Kamenovac",
+      "x": 18239.2,
+      "y": 6052.36
+    },
+    {
+      "name": "Belovgrad",
+      "x": 33606.5,
+      "y": 13950.4
+    },
+    {
+      "name": "Glišići",
+      "x": 8725.33,
+      "y": 11617.0
+    },
+    {
+      "name": "Bogdaš",
+      "x": 8035.36,
+      "y": 28388.7
+    },
+    {
+      "name": "Treskovac",
+      "x": 10606.8,
+      "y": 9496.33
+    },
+    {
+      "name": "Bukovište",
+      "x": 5943.76,
+      "y": 23536.8
+    },
+    {
+      "name": "Belin",
+      "x": 5654.37,
+      "y": 18048.8
+    },
+    {
+      "name": "Hitmadin",
+      "x": 35288.0,
+      "y": 17874.2
+    },
+    {
+      "name": "Novi Brest",
+      "x": 34388.1,
+      "y": 26623.5
+    },
+    {
+      "name": "Drmno",
+      "x": 22594.6,
+      "y": 5371.85
+    },
+    {
+      "name": "Baćkovac",
+      "x": 5792.94,
+      "y": 16097.4
+    },
+    {
+      "name": "Kušići",
+      "x": 9464.77,
+      "y": 31360.8
+    },
+    {
+      "name": "Visoko",
+      "x": 18436.4,
+      "y": 35976.2
+    },
+    {
+      "name": "Atomsko sklonište",
+      "x": 19558.5,
+      "y": 36091.9
+    },
+    {
+      "name": "Mali Dol",
+      "x": 31103.4,
+      "y": 8930.91
+    },
+    {
+      "name": "Brodarevo",
+      "x": 14585.7,
+      "y": 35068.7
+    },
+    {
+      "name": "Kamik",
+      "x": 29440.6,
+      "y": 7286.59
+    },
+    {
+      "name": "Prisjan",
+      "x": 29341.4,
+      "y": 7028.07
+    },
+    {
+      "name": "Milančevac",
+      "x": 19547.0,
+      "y": 4175.0
+    },
+    {
+      "name": "Rasjan",
+      "x": 29318.4,
+      "y": 6724.92
+    },
+    {
+      "name": "Kalna",
+      "x": 27324.6,
+      "y": 5631.55
+    },
+    {
+      "name": "Rakari",
+      "x": 5136.67,
+      "y": 26377.6
+    },
+    {
+      "name": "Markovci",
+      "x": 17715.5,
+      "y": 4229.36
+    },
+    {
+      "name": "Veliki Dol",
+      "x": 31486.3,
+      "y": 8148.24
+    },
+    {
+      "name": "Bokigrad",
+      "x": 18548.9,
+      "y": 36920.9
+    },
+    {
+      "name": "Mokrin",
+      "x": 26607.4,
+      "y": 35878.4
+    },
+    {
+      "name": "Javorje",
+      "x": 29393.4,
+      "y": 6121.89
+    },
+    {
+      "name": "Rakovica",
+      "x": 31197.0,
+      "y": 33690.0
+    },
+    {
+      "name": "Degmanovac gornji",
+      "x": 6537.35,
+      "y": 30610.9
+    },
+    {
+      "name": "Krupac",
+      "x": 14255.4,
+      "y": 36612.4
+    },
+    {
+      "name": "Bojanići",
+      "x": 21734.1,
+      "y": 3005.78
+    },
+    {
+      "name": "Šakalovac",
+      "x": 14183.8,
+      "y": 3978.28
+    },
+    {
+      "name": "Toplica",
+      "x": 15741.2,
+      "y": 3449.09
+    },
+    {
+      "name": "Suva gora",
+      "x": 10388.5,
+      "y": 35166.6
+    },
+    {
+      "name": "Zagubica",
+      "x": 2844.29,
+      "y": 23272.9
+    },
+    {
+      "name": "Lutonovac",
+      "x": 12632.2,
+      "y": 4368.0
+    },
+    {
+      "name": "Kuti",
+      "x": 28778.4,
+      "y": 4436.55
+    },
+    {
+      "name": "Gusan",
+      "x": 4607.47,
+      "y": 29171.0
+    },
+    {
+      "name": "Petnica",
+      "x": 3768.47,
+      "y": 28456.7
+    },
+    {
+      "name": "Kovačica",
+      "x": 31350.3,
+      "y": 35592.5
+    },
+    {
+      "name": "Ćalije",
+      "x": 6891.39,
+      "y": 7549.84
+    },
+    {
+      "name": "Stajevo",
+      "x": 1962.96,
+      "y": 23776.6
+    },
+    {
+      "name": "Kukinci",
+      "x": 26067.8,
+      "y": 38988.4
+    },
+    {
+      "name": "Klinci",
+      "x": 2638.3,
+      "y": 27989.7
+    },
+    {
+      "name": "Marijas",
+      "x": 20986.0,
+      "y": 40021.6
+    },
+    {
+      "name": "Arminovac",
+      "x": 30528.7,
+      "y": 37278.5
+    },
+    {
+      "name": "Japaga",
+      "x": 39265.4,
+      "y": 26069.1
+    },
+    {
+      "name": "Stapar",
+      "x": 38204.4,
+      "y": 29007.2
+    },
+    {
+      "name": "Pičkovac",
+      "x": 38588.1,
+      "y": 12621.1
+    },
+    {
+      "name": "Rajac",
+      "x": 27081.4,
+      "y": 39104.9
+    },
+    {
+      "name": "Degmanovac donji",
+      "x": 4643.02,
+      "y": 32340.5
+    },
+    {
+      "name": "Mrsinci",
+      "x": 27851.7,
+      "y": 39019.9
+    },
+    {
+      "name": "Babin gornji",
+      "x": 7454.16,
+      "y": 5257.41
+    },
+    {
+      "name": "Izgubići",
+      "x": 36050.2,
+      "y": 33354.1
+    },
+    {
+      "name": "Busur",
+      "x": 31651.4,
+      "y": 3298.53
+    },
+    {
+      "name": "Ada",
+      "x": 29277.9,
+      "y": 39182.6
+    },
+    {
+      "name": "Acidovac",
+      "x": 4509.85,
+      "y": 7241.91
+    },
+    {
+      "name": "Dolac",
+      "x": 7118.73,
+      "y": 36363.7
+    },
+    {
+      "name": "Kiseljak",
+      "x": 10157.3,
+      "y": 2445.87
+    },
+    {
+      "name": "Tinovac",
+      "x": 3226.79,
+      "y": 32156.8
+    },
+    {
+      "name": "Lisjan",
+      "x": 33367.4,
+      "y": 3853.18
+    },
+    {
+      "name": "Gorica",
+      "x": 33144.3,
+      "y": 37509.7
+    },
+    {
+      "name": "Bukvić",
+      "x": 12918.6,
+      "y": 645.78
+    },
+    {
+      "name": "Dubovo",
+      "x": 31220.3,
+      "y": 2038.23
+    },
+    {
+      "name": "Trstenica",
+      "x": 31328.1,
+      "y": 38871.7
+    },
+    {
+      "name": "Golubinci",
+      "x": 3999.61,
+      "y": 34294.3
+    },
+    {
+      "name": "Stapar",
+      "x": 35608.4,
+      "y": 36051.6
+    },
+    {
+      "name": "Babin donji",
+      "x": 5103.19,
+      "y": 4488.66
+    },
+    {
+      "name": "Divci",
+      "x": 38175.3,
+      "y": 34666.6
+    },
+    {
+      "name": "Ziginovo",
+      "x": 1901.15,
+      "y": 34126.8
+    },
+    {
+      "name": "Mol",
+      "x": 37894.0,
+      "y": 35732.2
+    },
+    {
+      "name": "Ratkovo",
+      "x": 33556.4,
+      "y": 39634.7
+    },
+    {
+      "name": "Vrba",
+      "x": 1037.74,
+      "y": 7514.47
+    },
+    {
+      "name": "Popinci",
+      "x": 2705.06,
+      "y": 37751.0
+    }
+  ]
+};
 Arma3Map.Maps.dingor = {
   CRS: MGRS_CRS(0.0315, 0.0315, 323),
   "worldName": "dingor",
@@ -3455,26 +4206,6 @@ Arma3Map.Maps.fata = {
       "y": 6985.35
     }
   ]
-};
-Arma3Map.Maps.gossi = {
-  CRS: MGRS_CRS(0.0039375, 0.0039375, 323),
-  "worldName": "gossi",
-  "tilePattern": "/maps/gossi/{z}/{x}/{y}.png",
-  "maxZoom": 7,
-  "minZoom": 0,
-  "defaultZoom": 3,
-  "attribution": "&copy; Bohemia Interactive",
-  "tileSize": 323,
-  "center": [
-    40960,
-    40960
-  ],
-  "worldSize": 81920.0,
-  "preview": null,
-  "dlc": null,
-  "title": "GOSSI, Mali",
-  "steamWorkshop": null,
-  "cities": []
 };
 Arma3Map.Maps.gulfcoast = {
   CRS: MGRS_CRS(0.01575, 0.01575, 242),
