a95f76491c764d95444e9c8fe7ba4a37051a6e45
Julien Etelain <julien.etelain@outlook.com>
2022-08-16 15:01:13 +0200

    G.O.S Leskovets #28

---
+++ maps/pja314.js
@@ -0,0 +1,216 @@
+Arma3Map.Maps.pja314 = {
+  CRS: MGRS_CRS(0.01575, 0.01575, 323),
+  "worldName": "pja314",
+  "tilePattern": "/maps/pja314/{z}/{x}/{y}.png",
+  "maxZoom": 6,
+  "minZoom": 0,
+  "defaultZoom": 2,
+  "attribution": "&copy; Bohemia Interactive, GOS Makhno",
+  "tileSize": 323,
+  "center": [
+    10240,
+    10240
+  ],
+  "worldSize": 20480.0,
+  "preview": "https://steamuserimages-a.akamaihd.net/ugc/155774065785146372/84F4CE205EEE47BEB46762616514A65129F420C9/",
+  "dlc": null,
+  "title": "G.O.S Leskovets",
+  "steamWorkshop": "https://steamcommunity.com/sharedfiles/filedetails/?id=855464203",
+  "cities": [
+    {
+      "name": "Izvos",
+      "x": 11744.0,
+      "y": 10578.0
+    },
+    {
+      "name": "Karbinti",
+      "x": 12192.0,
+      "y": 12992.0
+    },
+    {
+      "name": "Prevalna",
+      "x": 12613.0,
+      "y": 7925.0
+    },
+    {
+      "name": "Medovnitza",
+      "x": 7829.0,
+      "y": 12373.0
+    },
+    {
+      "name": "Krachimir",
+      "x": 10761.0,
+      "y": 6896.0
+    },
+    {
+      "name": "Varbotchev",
+      "x": 14968.0,
+      "y": 11149.0
+    },
+    {
+      "name": "Dalgo Pole",
+      "x": 10697.0,
+      "y": 14945.0
+    },
+    {
+      "name": "Rabisha",
+      "x": 7248.0,
+      "y": 14707.0
+    },
+    {
+      "name": "Granichak",
+      "x": 7559.0,
+      "y": 5890.0
+    },
+    {
+      "name": "Silvonik",
+      "x": 5581.0,
+      "y": 13183.0
+    },
+    {
+      "name": "Gorna Luka",
+      "x": 16226.0,
+      "y": 7833.0
+    },
+    {
+      "name": "Shipol",
+      "x": 14923.0,
+      "y": 15105.0
+    },
+    {
+      "name": "Makresh",
+      "x": 8782.0,
+      "y": 16888.0
+    },
+    {
+      "name": "Yanyovets",
+      "x": 17484.0,
+      "y": 10074.0
+    },
+    {
+      "name": "Prauzhda",
+      "x": 4152.0,
+      "y": 8931.0
+    },
+    {
+      "name": "Kireevo",
+      "x": 3867.0,
+      "y": 12402.0
+    },
+    {
+      "name": "Slatina",
+      "x": 12244.0,
+      "y": 3560.0
+    },
+    {
+      "name": "Izvor",
+      "x": 17416.0,
+      "y": 14537.0
+    },
+    {
+      "name": "Rakovista",
+      "x": 13798.0,
+      "y": 18140.0
+    },
+    {
+      "name": "Diva Slatina",
+      "x": 8196.0,
+      "y": 2843.0
+    },
+    {
+      "name": "Varba",
+      "x": 3337.0,
+      "y": 14308.0
+    },
+    {
+      "name": "Brankovitsi",
+      "x": 11335.0,
+      "y": 19109.0
+    },
+    {
+      "name": "Stakesvit",
+      "x": 19199.0,
+      "y": 7993.0
+    },
+    {
+      "name": "Gramada",
+      "x": 5762.0,
+      "y": 17812.0
+    },
+    {
+      "name": "Valkesh",
+      "x": 16492.0,
+      "y": 17354.0
+    },
+    {
+      "name": "Berkovitsa",
+      "x": 10073.0,
+      "y": 1684.0
+    },
+    {
+      "name": "Salash",
+      "x": 2802.0,
+      "y": 6484.0
+    },
+    {
+      "name": "Vartop",
+      "x": 1790.0,
+      "y": 11954.0
+    },
+    {
+      "name": "Barzia",
+      "x": 14826.0,
+      "y": 2366.0
+    },
+    {
+      "name": "Varbono",
+      "x": 18559.0,
+      "y": 5226.0
+    },
+    {
+      "name": "Zamfirovo",
+      "x": 13714.0,
+      "y": 542.0
+    },
+    {
+      "name": "Oshane",
+      "x": 1319.0,
+      "y": 15797.0
+    },
+    {
+      "name": "Leskovets",
+      "x": 18144.0,
+      "y": 2354.0
+    },
+    {
+      "name": "Damyanovo",
+      "x": 2368.0,
+      "y": 3077.0
+    },
+    {
+      "name": "Kostensi",
+      "x": 4013.0,
+      "y": 1425.0
+    },
+    {
+      "name": "Balyuvitsa",
+      "x": 17768.0,
+      "y": 1154.0
+    },
+    {
+      "name": "Kosta",
+      "x": 2193.0,
+      "y": 18635.0
+    },
+    {
+      "name": "Kladorub",
+      "x": 18463.0,
+      "y": 20002.0
+    },
+    {
+      "name": "Zhelezna",
+      "x": 166.0,
+      "y": 1584.0
+    }
+  ]
+};
