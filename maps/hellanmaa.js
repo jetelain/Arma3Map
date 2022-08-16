9fcad95d923248bc6b9ec34a83c69256673c2d83
Julien Etelain <julien.etelain@outlook.com>
2022-08-16 15:04:42 +0200

    Hellanmaa #29

---
+++ maps/hellanmaa.js
@@ -0,0 +1,111 @@
+Arma3Map.Maps.hellanmaa = {
+  CRS: MGRS_CRS(0.0315, 0.0315, 259),
+  "worldName": "hellanmaa",
+  "tilePattern": "/maps/hellanmaa/{z}/{x}/{y}.png",
+  "maxZoom": 5,
+  "minZoom": 0,
+  "defaultZoom": 2,
+  "attribution": "&copy; Bohemia Interactive, Temppa",
+  "tileSize": 259,
+  "center": [
+    4096,
+    4096
+  ],
+  "worldSize": 8192.0,
+  "preview": "https://steamuserimages-a.akamaihd.net/ugc/766100671613747219/A8D5C8D5F0081C7B437BE8EC86320C64BAAFCF41/",
+  "dlc": null,
+  "title": "Hellanmaa",
+  "steamWorkshop": "https://steamcommunity.com/sharedfiles/filedetails/?id=1291778160",
+  "cities": [
+    {
+      "name": "Somppi",
+      "x": 3671.91,
+      "y": 4959.02
+    },
+    {
+      "name": "Hööpakka",
+      "x": 4477.26,
+      "y": 3097.97
+    },
+    {
+      "name": "Nyman",
+      "x": 3614.89,
+      "y": 2739.61
+    },
+    {
+      "name": "Kangas",
+      "x": 6379.1,
+      "y": 3631.21
+    },
+    {
+      "name": "Kokkila",
+      "x": 1744.55,
+      "y": 4381.99
+    },
+    {
+      "name": "Juusola",
+      "x": 5714.66,
+      "y": 2239.68
+    },
+    {
+      "name": "Koivikko",
+      "x": 3828.52,
+      "y": 6949.44
+    },
+    {
+      "name": "Troihari",
+      "x": 1433.23,
+      "y": 3019.2
+    },
+    {
+      "name": "Huhtala",
+      "x": 6889.9,
+      "y": 2647.93
+    },
+    {
+      "name": "Luoma",
+      "x": 6235.93,
+      "y": 6781.47
+    },
+    {
+      "name": "Taipale",
+      "x": 7214.69,
+      "y": 5550.09
+    },
+    {
+      "name": "Jankos industrial area",
+      "x": 2534.91,
+      "y": 970.66
+    },
+    {
+      "name": "Kettula",
+      "x": 5551.55,
+      "y": 901.966
+    },
+    {
+      "name": "Heikkola",
+      "x": 3696.09,
+      "y": 7763.39
+    },
+    {
+      "name": "Kouras metal",
+      "x": 5589.31,
+      "y": 673.45
+    },
+    {
+      "name": "Kontio",
+      "x": 7795.29,
+      "y": 3240.44
+    },
+    {
+      "name": "Rautakorpi",
+      "x": 752.514,
+      "y": 6470.37
+    },
+    {
+      "name": "Jussila",
+      "x": 7420.74,
+      "y": 7381.69
+    }
+  ]
+};
