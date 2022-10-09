9cfb16b7f9011fbc3308dc475a1fe7d7f2cac40a
BigManJapans <115350273+BigManJapans@users.noreply.github.com>
2022-10-09 06:46:08 -0400

    BELAVEZHSK (#34)
    Added BELAVEZHSK.

---
+++ maps/belavezhsk.js
@@ -0,0 +1,181 @@
+Arma3Map.Maps.belavezhsk = {
+  CRS: MGRS_CRS(0.0105, 0.0105, 211),
+  "worldName": "belavezhsk",
+  "tilePattern": "/maps/belavezhsk/{z}/{x}/{y}.png",
+  "maxZoom": 7,
+  "minZoom": 0,
+  "defaultZoom": 3,
+  "attribution": "&copy; Bohemia Interactive",
+  "tileSize": 211,
+  "center": [
+    10035,
+    10035
+  ],
+  "worldSize": 20070.4,
+  "preview": null,
+  "dlc": null,
+  "title": "Belavezhsk",
+  "steamWorkshop": null,
+  "cities": [
+    {
+      "name": "WF-2",
+      "x": 4322.78,
+      "y": 5577.95
+    },
+    {
+      "name": "CHP-1",
+      "x": 3542.31,
+      "y": 5546.58
+    },
+    {
+      "name": "BELAVEZHSK",
+      "x": 3284.96,
+      "y": 5402.96
+    },
+    {
+      "name": "Central Regional Hospital",
+      "x": 3215.87,
+      "y": 5566.62
+    },
+    {
+      "name": "Water company",
+      "x": 2771.27,
+      "y": 5079.03
+    },
+    {
+      "name": "Juravinka",
+      "x": 6425.48,
+      "y": 7336.98
+    },
+    {
+      "name": "Dubrovka",
+      "x": 8197.4,
+      "y": 4347.23
+    },
+    {
+      "name": "Lesnoe-17",
+      "x": 3779.89,
+      "y": 2094.92
+    },
+    {
+      "name": "State museum of folk architecture and rural lifestyle",
+      "x": 4357.15,
+      "y": 1055.19
+    },
+    {
+      "name": "Belavezhsk sugar plant",
+      "x": 1918.53,
+      "y": 2124.62
+    },
+    {
+      "name": "Millitary unit 4214",
+      "x": 8303.56,
+      "y": 2027.73
+    },
+    {
+      "name": "Savichi",
+      "x": 8268.31,
+      "y": 1602.49
+    },
+    {
+      "name": "Novoe selo",
+      "x": 4859.23,
+      "y": 10174.9
+    },
+    {
+      "name": "Agrokombinat «Paudnevy»",
+      "x": 1461.73,
+      "y": 8889.09
+    },
+    {
+      "name": "Novy Dvor",
+      "x": 678.377,
+      "y": 8847.43
+    },
+    {
+      "name": "SPP-4",
+      "x": 11029.9,
+      "y": 4190.01
+    },
+    {
+      "name": "Military training area",
+      "x": 10801.1,
+      "y": 7088.86
+    },
+    {
+      "name": "Teplin",
+      "x": 10309.9,
+      "y": 8487.89
+    },
+    {
+      "name": "3rd Guards Red Banner Air Base",
+      "x": 568.339,
+      "y": 590.083
+    },
+    {
+      "name": "Mobile mechanized column",
+      "x": 1094.34,
+      "y": 11825.6
+    },
+    {
+      "name": "Krasni Bor",
+      "x": 6149.69,
+      "y": 12891.9
+    },
+    {
+      "name": "Belavezhskdrev",
+      "x": 4138.69,
+      "y": 14125.4
+    },
+    {
+      "name": "Polessky state radiation-ecological reserve",
+      "x": 15205.4,
+      "y": 6037.82
+    },
+    {
+      "name": "Dairy complex",
+      "x": 633.605,
+      "y": 14269.4
+    },
+    {
+      "name": "Borki",
+      "x": 1078.17,
+      "y": 14618.3
+    },
+    {
+      "name": "Oleshevichi",
+      "x": 6781.88,
+      "y": 17572.6
+    },
+    {
+      "name": "Liadi",
+      "x": 5543.27,
+      "y": 17958.5
+    },
+    {
+      "name": "Wagon repair plant",
+      "x": 1540.47,
+      "y": 17622.8
+    },
+    {
+      "name": "Ozarichi",
+      "x": 13054.4,
+      "y": 16611.4
+    },
+    {
+      "name": "Riasna",
+      "x": 1447.92,
+      "y": 18854.2
+    },
+    {
+      "name": "Malinovka",
+      "x": 15686.7,
+      "y": 14871.5
+    },
+    {
+      "name": "Regional Airport ",
+      "x": 17157.8,
+      "y": 18557.2
+    }
+  ]
+};
