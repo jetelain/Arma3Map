be97933f47bed5b53391d5133337630606b85f84
DNico-97 <dnicolson97@gmail.com>
2023-04-22 14:57:48 +0100

    Added 5 maps
    Diego Garcia https://steamcommunity.com/sharedfiles/filedetails/?id=385314016
    Tembelan https://steamcommunity.com/sharedfiles/filedetails/?id=1252091296
    Senkaku: https://steamcommunity.com/sharedfiles/filedetails/?id=1544030265
    Wake: https://steamcommunity.com/sharedfiles/filedetails/?id=339479337
    Archipelago: https://steamcommunity.com/sharedfiles/filedetails/?id=1726184748

---
+++ maps/archipelago.js
@@ -0,0 +1,246 @@
+Arma3Map.Maps.archipelago = {
+  CRS: MGRS_CRS(0.0315, 0.0315, 259),
+  "worldName": "archipelago",
+  "tilePattern": "/maps/archipelago/{z}/{x}/{y}.png",
+  "maxZoom": 5,
+  "minZoom": 0,
+  "defaultZoom": 2,
+  "attribution": "&copy; Bohemia Interactive",
+  "tileSize": 259,
+  "center": [
+    4096,
+    4096
+  ],
+  "worldSize": 8192.0,
+  "preview": null,
+  "dlc": null,
+  "title": "Archipelago",
+  "steamWorkshop": null,
+  "cities": [
+    {
+      "name": "Tiba",
+      "x": 4751.86,
+      "y": 3937.05
+    },
+    {
+      "name": "Ktuk",
+      "x": 3540.57,
+      "y": 4608.53
+    },
+    {
+      "name": "Bellcoe Port",
+      "x": 3417.9,
+      "y": 4441.53
+    },
+    {
+      "name": "Port Tyva",
+      "x": 4694.62,
+      "y": 4619.73
+    },
+    {
+      "name": "Nantoon Ore Mine",
+      "x": 3184.03,
+      "y": 4473.31
+    },
+    {
+      "name": "Doraz",
+      "x": 3492.17,
+      "y": 5051.86
+    },
+    {
+      "name": "Wnaskas",
+      "x": 3172.45,
+      "y": 4902.1
+    },
+    {
+      "name": "Brookthunna",
+      "x": 5032.14,
+      "y": 4909.46
+    },
+    {
+      "name": "Axhull",
+      "x": 5143.05,
+      "y": 3398.43
+    },
+    {
+      "name": "Supply Wharf",
+      "x": 4320.17,
+      "y": 2835.29
+    },
+    {
+      "name": "Rison Resort",
+      "x": 2945.22,
+      "y": 3207.42
+    },
+    {
+      "name": "Lequi",
+      "x": 2634.27,
+      "y": 4149.41
+    },
+    {
+      "name": "Wapierre",
+      "x": 5320.81,
+      "y": 3277.04
+    },
+    {
+      "name": "La Coe",
+      "x": 2978.86,
+      "y": 5244.55
+    },
+    {
+      "name": "Ranma",
+      "x": 5267.63,
+      "y": 5697.89
+    },
+    {
+      "name": "Saint Bernorth",
+      "x": 6066.0,
+      "y": 3526.12
+    },
+    {
+      "name": "Port Orines",
+      "x": 5207.05,
+      "y": 1817.28
+    },
+    {
+      "name": "Thesa",
+      "x": 6603.63,
+      "y": 3133.39
+    },
+    {
+      "name": "Libad",
+      "x": 3272.88,
+      "y": 6751.68
+    },
+    {
+      "name": "Port Bourclear",
+      "x": 3944.72,
+      "y": 6890.53
+    },
+    {
+      "name": "Winbra",
+      "x": 1668.41,
+      "y": 5513.98
+    },
+    {
+      "name": "Thetra",
+      "x": 2170.86,
+      "y": 6311.43
+    },
+    {
+      "name": "Elth",
+      "x": 6782.61,
+      "y": 2722.02
+    },
+    {
+      "name": "La Char",
+      "x": 5092.08,
+      "y": 1215.71
+    },
+    {
+      "name": "Ruswan",
+      "x": 3339.22,
+      "y": 7155.34
+    },
+    {
+      "name": "Tymur",
+      "x": 5592.95,
+      "y": 1277.34
+    },
+    {
+      "name": "Chilqui",
+      "x": 4133.08,
+      "y": 7302.38
+    },
+    {
+      "name": "Byma",
+      "x": 1252.94,
+      "y": 5627.22
+    },
+    {
+      "name": "La Char Mine",
+      "x": 5055.13,
+      "y": 996.56
+    },
+    {
+      "name": "West Bornebou",
+      "x": 7027.26,
+      "y": 2517.51
+    },
+    {
+      "name": "Nideer",
+      "x": 2199.95,
+      "y": 6957.73
+    },
+    {
+      "name": "Saint Runornia",
+      "x": 7389.7,
+      "y": 2904.58
+    },
+    {
+      "name": "Bornebou",
+      "x": 7242.37,
+      "y": 2524.96
+    },
+    {
+      "name": "Grand Del",
+      "x": 6219.83,
+      "y": 1209.32
+    },
+    {
+      "name": "Equirock",
+      "x": 7721.45,
+      "y": 3503.67
+    },
+    {
+      "name": "Grand Mial",
+      "x": 6536.37,
+      "y": 6905.43
+    },
+    {
+      "name": "Tsontoche",
+      "x": 7233.34,
+      "y": 1644.35
+    },
+    {
+      "name": "Laqa",
+      "x": 5851.15,
+      "y": 484.14
+    },
+    {
+      "name": "La Mera",
+      "x": 680.86,
+      "y": 6364.46
+    },
+    {
+      "name": "Azla",
+      "x": 6500.06,
+      "y": 759.68
+    },
+    {
+      "name": "Port Stosran",
+      "x": 7472.42,
+      "y": 1747.38
+    },
+    {
+      "name": "Rinesco",
+      "x": 1156.27,
+      "y": 6983.05
+    },
+    {
+      "name": "Terktuk",
+      "x": 7517.34,
+      "y": 6505.51
+    },
+    {
+      "name": "Viskam Wind Farm",
+      "x": 7392.08,
+      "y": 6915.51
+    },
+    {
+      "name": "Vanhav",
+      "x": 1140.73,
+      "y": 7427.85
+    }
+  ]
+};
