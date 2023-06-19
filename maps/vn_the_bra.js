c71c5e4426d1b91902d623247a574f7b7f65bbe7
Julien Etelain <julien.etelain@outlook.com>
2023-06-19 16:32:38 +0200

    "The Bra" #57

---
+++ maps/vn_the_bra.js
@@ -0,0 +1,81 @@
+Arma3Map.Maps.vn_the_bra = {
+  CRS: MGRS_CRS(0.063, 0.063, 323),
+  "worldName": "vn_the_bra",
+  "tilePattern": "/maps/vn_the_bra/{z}/{x}/{y}.png",
+  "maxZoom": 4,
+  "minZoom": 0,
+  "defaultZoom": 2,
+  "attribution": "&copy; Bohemia Interactive",
+  "tileSize": 323,
+  "center": [
+    2560,
+    2560
+  ],
+  "worldSize": 5120.0,
+  "preview": null,
+  "dlc": null,
+  "title": "The Bra",
+  "steamWorkshop": null,
+  "cities": [
+    {
+      "name": "Pia Hom",
+      "x": 2289.13,
+      "y": 392.0
+    },
+    {
+      "name": "Pak Ha",
+      "x": 1525.68,
+      "y": 2410.53
+    },
+    {
+      "name": "Antoum",
+      "x": 759.73,
+      "y": 2550.12
+    },
+    {
+      "name": "Ban Onglouang",
+      "x": 735.08,
+      "y": 3300.58
+    },
+    {
+      "name": "Ban Pakha",
+      "x": 2010.96,
+      "y": 3388.41
+    },
+    {
+      "name": "Vangtat Kang",
+      "x": 2481.8,
+      "y": 3257.76
+    },
+    {
+      "name": "Ban Chom",
+      "x": 3801.75,
+      "y": 1894.39
+    },
+    {
+      "name": "Nam Phat",
+      "x": 3718.31,
+      "y": 2908.54
+    },
+    {
+      "name": "Ban Phiadouang",
+      "x": 3570.61,
+      "y": 3293.12
+    },
+    {
+      "name": "Ban Dac Maruk",
+      "x": 1331.82,
+      "y": 4480.22
+    },
+    {
+      "name": "Phouvong",
+      "x": 479.36,
+      "y": 4526.7
+    },
+    {
+      "name": "Xayden",
+      "x": 4787.96,
+      "y": 2752.69
+    }
+  ]
+};
