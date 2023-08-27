88ffe1f497acf3945ee9638e44e660ec56a10351
Julien Etelain <julien.etelain@outlook.com>
2023-08-27 15:09:40 +0200

    Sa'hatra #70

---
+++ maps/juju_sahatra.js
@@ -0,0 +1,66 @@
+Arma3Map.Maps.juju_sahatra = {
+  CRS: MGRS_CRS(0.0315, 0.0315, 259),
+  "worldName": "juju_sahatra",
+  "tilePattern": "/maps/juju_sahatra/{z}/{x}/{y}.png",
+  "maxZoom": 5,
+  "minZoom": 0,
+  "defaultZoom": 2,
+  "attribution": "&copy; Bohemia Interactive",
+  "tileSize": 259,
+  "center": [
+    4096,
+    4096
+  ],
+  "worldSize": 8192.0,
+  "preview": "https://steamuserimages-a.akamaihd.net/ugc/2061012395722264947/B9912B84F2D6AFFE7BB2168B06E62503FFBDEECE/",
+  "dlc": null,
+  "title": "Sa'hatra",
+  "steamWorkshop": "https://steamcommunity.com/sharedfiles/filedetails/?id=3019928771",
+  "cities": [
+    {
+      "name": "Uyahat",
+      "x": 1454.98,
+      "y": 3850.75
+    },
+    {
+      "name": "Zakrit",
+      "x": 5057.55,
+      "y": 853.82
+    },
+    {
+      "name": "Western Sa'hatra",
+      "x": 5138.47,
+      "y": 3024.75
+    },
+    {
+      "name": "Nisul",
+      "x": 2581.67,
+      "y": 6368.4
+    },
+    {
+      "name": "Sa'hatra",
+      "x": 6523.66,
+      "y": 3267.49
+    },
+    {
+      "name": "Eastern Sa'hatra",
+      "x": 6897.84,
+      "y": 1805.44
+    },
+    {
+      "name": "Sufahan",
+      "x": 5725.47,
+      "y": 5524.33
+    },
+    {
+      "name": "Ardaft",
+      "x": 8035.55,
+      "y": 1270.91
+    },
+    {
+      "name": "Farshut",
+      "x": 7337.81,
+      "y": 6653.13
+    }
+  ]
+};
