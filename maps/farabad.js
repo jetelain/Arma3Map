21c67650d32a12ca3b3db282edc035a4a3a12cd9
Julien Etelain <julien.etelain@outlook.com>
2023-11-12 15:16:55 +0100

    Farabad #75

--- maps/farabad.js
+++ maps/farabad.js
@@ -1,12 +1,12 @@
 Arma3Map.Maps.farabad = {
-  CRS: MGRS_CRS(0.02475, 0.0253125, 273),
+  CRS: MGRS_CRS(0.0315, 0.0315, 339),
   "worldName": "farabad",
   "tilePattern": "/maps/farabad/{z}/{x}/{y}.png",
   "maxZoom": 5,
   "minZoom": 0,
   "defaultZoom": 2,
   "attribution": "&copy; Bohemia Interactive",
-  "tileSize": 273,
+  "tileSize": 339,
   "center": [
     5376,
     5376
@@ -29,8 +29,13 @@ Arma3Map.Maps.farabad = {
     },
     {
       "name": "Farabad",
-      "x": 4534.29,
-      "y": 5186.77
+      "x": 4515.79,
+      "y": 5146.41
+    },
+    {
+      "name": "Kaftar Kar",
+      "x": 9094.97,
+      "y": 10435.5
     },
     {
       "name": "GREEN ZONE",
@@ -38,9 +43,34 @@ Arma3Map.Maps.farabad = {
       "y": 4948.94
     },
     {
+      "name": "Shahruk",
+      "x": 8749.23,
+      "y": 7238.22
+    },
+    {
+      "name": "Kandah",
+      "x": 9591.41,
+      "y": 9081.76
+    },
+    {
+      "name": "Ali Kala",
+      "x": 8880.51,
+      "y": 6014.01
+    },
+    {
       "name": "Hamza",
       "x": 8487.0,
       "y": 3437.78
+    },
+    {
+      "name": "Karkanak",
+      "x": 2330.22,
+      "y": 598.98
+    },
+    {
+      "name": "Port Farabad",
+      "x": 7979.58,
+      "y": 659.33
     }
   ]
 };
