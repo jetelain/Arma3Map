d933d162ce96176a94f022be2ea2a282962fa9fa
Julien Etelain <julien.etelain@outlook.com>
2024-04-19 20:08:48 +0200

    Green sea  #76

---
+++ maps/green_sea.js
@@ -0,0 +1,321 @@
+Arma3Map.Maps.green_sea = {
+  CRS: MGRS_CRS(0.01575, 0.01575, 323),
+  "worldName": "green_sea",
+  "tilePattern": "/maps/green_sea/{z}/{x}/{y}.png",
+  "maxZoom": 6,
+  "minZoom": 0,
+  "defaultZoom": 2,
+  "attribution": "&copy; Bohemia Interactive",
+  "tileSize": 323,
+  "center": [
+    10240,
+    10240
+  ],
+  "worldSize": 20480.0,
+  "preview": null,
+  "dlc": null,
+  "title": "Green Sea",
+  "steamWorkshop": null,
+  "cities": [
+    {
+      "name": "Pusta",
+      "x": 8647.18,
+      "y": 10098.5
+    },
+    {
+      "name": "Staroye",
+      "x": 9732.52,
+      "y": 11529.0
+    },
+    {
+      "name": "Kamyshovo",
+      "x": 11583.1,
+      "y": 9659.58
+    },
+    {
+      "name": "Elektrozavodsk",
+      "x": 9833.72,
+      "y": 8303.41
+    },
+    {
+      "name": "Msta",
+      "x": 10853.8,
+      "y": 11565.4
+    },
+    {
+      "name": "Tulga",
+      "x": 12322.5,
+      "y": 10513.5
+    },
+    {
+      "name": "Prigorodki",
+      "x": 7535.58,
+      "y": 9563.26
+    },
+    {
+      "name": "Shakhovka",
+      "x": 9248.64,
+      "y": 12711.3
+    },
+    {
+      "name": "Dolina",
+      "x": 10805.2,
+      "y": 12777.6
+    },
+    {
+      "name": "Mogilevka",
+      "x": 7170.41,
+      "y": 11240.5
+    },
+    {
+      "name": "Guglovo",
+      "x": 7951.08,
+      "y": 12847.2
+    },
+    {
+      "name": "Orlovets",
+      "x": 11651.0,
+      "y": 13333.8
+    },
+    {
+      "name": "Solnitchniy",
+      "x": 12917.4,
+      "y": 12480.8
+    },
+    {
+      "name": "Tchernogorsk",
+      "x": 6251.21,
+      "y": 8698.13
+    },
+    {
+      "name": "Polana",
+      "x": 10227.1,
+      "y": 14197.3
+    },
+    {
+      "name": "Vishnoye",
+      "x": 6081.91,
+      "y": 12222.8
+    },
+    {
+      "name": "Nadezhdino",
+      "x": 5396.66,
+      "y": 10840.9
+    },
+    {
+      "name": "Nizhnoye",
+      "x": 12344.9,
+      "y": 14241.7
+    },
+    {
+      "name": "Gorka",
+      "x": 9136.06,
+      "y": 15058.4
+    },
+    {
+      "name": "Novy Sobor",
+      "x": 6643.63,
+      "y": 13918.6
+    },
+    {
+      "name": "Berezino",
+      "x": 11534.0,
+      "y": 15234.8
+    },
+    {
+      "name": "Pulkovo",
+      "x": 4470.82,
+      "y": 11771.1
+    },
+    {
+      "name": "Stary Sobor",
+      "x": 5689.01,
+      "y": 13937.7
+    },
+    {
+      "name": "Kozlovka",
+      "x": 3997.57,
+      "y": 10741.8
+    },
+    {
+      "name": "Dubrovka",
+      "x": 9902.13,
+      "y": 16082.3
+    },
+    {
+      "name": "Balota",
+      "x": 4047.58,
+      "y": 8609.32
+    },
+    {
+      "name": "Rogovo",
+      "x": 4290.96,
+      "y": 12979.3
+    },
+    {
+      "name": "Pogorevka",
+      "x": 3955.36,
+      "y": 12569.6
+    },
+    {
+      "name": "Komarovo",
+      "x": 3184.31,
+      "y": 8643.0
+    },
+    {
+      "name": "Kabanino",
+      "x": 4820.71,
+      "y": 14789.9
+    },
+    {
+      "name": "Bor",
+      "x": 2846.74,
+      "y": 10030.7
+    },
+    {
+      "name": "Khelm",
+      "x": 11849.3,
+      "y": 16915.0
+    },
+    {
+      "name": "Drozhino",
+      "x": 2902.02,
+      "y": 11069.9
+    },
+    {
+      "name": "Kamenyy",
+      "x": 17535.0,
+      "y": 9994.0
+    },
+    {
+      "name": "Zelenogorsk",
+      "x": 2294.26,
+      "y": 11524.4
+    },
+    {
+      "name": "Grishino",
+      "x": 5479.94,
+      "y": 16479.7
+    },
+    {
+      "name": "Gvozdno",
+      "x": 8134.62,
+      "y": 18034.3
+    },
+    {
+      "name": "Sosnovka",
+      "x": 2059.26,
+      "y": 12502.7
+    },
+    {
+      "name": "Vybor",
+      "x": 3353.17,
+      "y": 15062.9
+    },
+    {
+      "name": "Krasnostav",
+      "x": 10744.3,
+      "y": 18418.5
+    },
+    {
+      "name": "Pustoshka",
+      "x": 2590.23,
+      "y": 14084.5
+    },
+    {
+      "name": "Utes Isle",
+      "x": 18649.4,
+      "y": 9799.42
+    },
+    {
+      "name": "Strelka",
+      "x": 18574.9,
+      "y": 8754.67
+    },
+    {
+      "name": "Kamenka",
+      "x": 1373.09,
+      "y": 8393.85
+    },
+    {
+      "name": "Pavlovo",
+      "x": 1213.98,
+      "y": 10002.7
+    },
+    {
+      "name": "Myshkino",
+      "x": 1518.18,
+      "y": 13499.1
+    },
+    {
+      "name": "Olsha",
+      "x": 12851.0,
+      "y": 19044.1
+    },
+    {
+      "name": "Zamok Dozornyy",
+      "x": 17747.8,
+      "y": 15743.7
+    },
+    {
+      "name": "Lopatino",
+      "x": 2270.0,
+      "y": 16149.0
+    },
+    {
+      "name": "Zelenomorsk",
+      "x": 17126.9,
+      "y": 17153.4
+    },
+    {
+      "name": "Petrovka",
+      "x": 4536.45,
+      "y": 18634.8
+    },
+    {
+      "name": "Veteran Isle",
+      "x": 14356.5,
+      "y": 623.063
+    },
+    {
+      "name": "Peregon",
+      "x": 13838.0,
+      "y": 20157.0
+    },
+    {
+      "name": "Okrainnyy",
+      "x": 18509.7,
+      "y": 17566.7
+    },
+    {
+      "name": "Svyatnyy",
+      "x": 20215.9,
+      "y": 15253.0
+    },
+    {
+      "name": "Poputniy",
+      "x": 15823.8,
+      "y": 19978.8
+    },
+    {
+      "name": "Primorskiy",
+      "x": 16945.4,
+      "y": 19505.1
+    },
+    {
+      "name": "Volkovo",
+      "x": 18583.6,
+      "y": 19364.3
+    },
+    {
+      "name": "Dubovo",
+      "x": 20092.6,
+      "y": 17732.3
+    },
+    {
+      "name": "Eastside Port",
+      "x": 18982.0,
+      "y": 916.0
+    }
+  ]
+};
