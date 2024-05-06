c16217b2ff68120cac67a8d7a501057183c00ad2
Julien Etelain <julien.etelain@outlook.com>
2024-05-06 11:43:09 +0200

    Kimmirut #83

---
+++ maps/sze_kimmirut.js
@@ -0,0 +1,26 @@
+Arma3Map.Maps.sze_kimmirut = {
+  CRS: MGRS_CRS(0.01575, 0.01575, 323),
+  "worldName": "sze_kimmirut",
+  "tilePattern": "/maps/sze_kimmirut/{z}/{x}/{y}.png",
+  "maxZoom": 6,
+  "minZoom": 0,
+  "defaultZoom": 2,
+  "attribution": "&copy; Bohemia Interactive",
+  "tileSize": 323,
+  "center": [
+    10240,
+    10240
+  ],
+  "worldSize": 20480.0,
+  "preview": null,
+  "dlc": null,
+  "title": "Kimmirut",
+  "steamWorkshop": null,
+  "cities": [
+    {
+      "name": "CFS Kimmirut",
+      "x": 16904.2,
+      "y": 17469.4
+    }
+  ]
+};
