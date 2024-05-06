7a7ad8ecf7614dbf0eb6cb76c19d296df6f18e83
Julien Etelain <julien.etelain@outlook.com>
2024-05-06 11:41:31 +0200

    Sanagasta #85

---
+++ maps/btrx_sngt.js
@@ -0,0 +1,20 @@
+Arma3Map.Maps.btrx_sngt = {
+  CRS: MGRS_CRS(0.01575, 0.01575, 323),
+  "worldName": "btrx_sngt",
+  "tilePattern": "/maps/btrx_sngt/{z}/{x}/{y}.png",
+  "maxZoom": 6,
+  "minZoom": 0,
+  "defaultZoom": 2,
+  "attribution": "&copy; Bohemia Interactive",
+  "tileSize": 323,
+  "center": [
+    10240,
+    10240
+  ],
+  "worldSize": 20480.0,
+  "preview": null,
+  "dlc": null,
+  "title": "Sanagasta, Argentina",
+  "steamWorkshop": null,
+  "cities": []
+};
