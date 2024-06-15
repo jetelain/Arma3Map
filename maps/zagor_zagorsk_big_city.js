25c7cce80efd7dc5951a906c48ae2d02568da0d8
PR2403 <64511917+PR2403@users.noreply.github.com>
2024-06-16 00:10:43 +0800

    Add files via upload

---
+++ maps/zagor_zagorsk_big_city.js
@@ -0,0 +1,276 @@
+Arma3Map.Maps.zagor_zagorsk_big_city = {
+  CRS: MGRS_CRS(0.0315, 0.0315, 323),
+  "worldName": "zagor_zagorsk_big_city",
+  "tilePattern": "/maps/zagor_zagorsk_big_city/{z}/{x}/{y}.png",
+  "maxZoom": 5,
+  "minZoom": 0,
+  "defaultZoom": 2,
+  "attribution": "&copy; Bohemia Interactive",
+  "tileSize": 323,
+  "center": [
+    5120,
+    5120
+  ],
+  "worldSize": 10240.0,
+  "preview": null,
+  "dlc": null,
+  "title": "Zagorsk Big City",
+  "steamWorkshop": null,
+  "cities": [
+    {
+      "name": "Zagorsk",
+      "x": 4263.84,
+      "y": 4520.0
+    },
+    {
+      "name": "Zaprudnyi",
+      "x": 7086.45,
+      "y": 4392.29
+    },
+    {
+      "name": "Ozeretskoe",
+      "x": 6613.83,
+      "y": 3047.58
+    },
+    {
+      "name": "Kupreevo",
+      "x": 6903.45,
+      "y": 7394.68
+    },
+    {
+      "name": "Petrovskoe",
+      "x": 7512.95,
+      "y": 3268.0
+    },
+    {
+      "name": "Vorontsovo",
+      "x": 7752.63,
+      "y": 7387.59
+    },
+    {
+      "name": "Litovka",
+      "x": 6989.84,
+      "y": 8052.55
+    },
+    {
+      "name": "Astakhovo",
+      "x": 7388.06,
+      "y": 7838.92
+    },
+    {
+      "name": "Ogudnevo",
+      "x": 1607.88,
+      "y": 4456.02
+    },
+    {
+      "name": "Liapino",
+      "x": 7945.13,
+      "y": 7862.59
+    },
+    {
+      "name": "Erzovka",
+      "x": 6713.34,
+      "y": 1472.53
+    },
+    {
+      "name": "Aleksino",
+      "x": 6053.48,
+      "y": 9175.09
+    },
+    {
+      "name": "Timonovo",
+      "x": 867.03,
+      "y": 5853.6
+    },
+    {
+      "name": "Tornovoe",
+      "x": 8381.27,
+      "y": 2155.5
+    },
+    {
+      "name": "Sandarovo",
+      "x": 664.28,
+      "y": 4801.68
+    },
+    {
+      "name": "Brutovo",
+      "x": 8256.33,
+      "y": 8349.57
+    },
+    {
+      "name": "Ryzhkovo",
+      "x": 3506.78,
+      "y": 9347.91
+    },
+    {
+      "name": "Kolpakovo",
+      "x": 2456.45,
+      "y": 8831.94
+    },
+    {
+      "name": "Neroshchino",
+      "x": 777.83,
+      "y": 6594.02
+    },
+    {
+      "name": "Svobodnyi",
+      "x": 1560.0,
+      "y": 2131.92
+    },
+    {
+      "name": "Dedenevo",
+      "x": 1860.08,
+      "y": 8442.99
+    },
+    {
+      "name": "Sadovka",
+      "x": 9630.64,
+      "y": 6282.57
+    },
+    {
+      "name": "Volodino",
+      "x": 8060.31,
+      "y": 8799.91
+    },
+    {
+      "name": "Shipovka",
+      "x": 3779.8,
+      "y": 530.94
+    },
+    {
+      "name": "Okhotino",
+      "x": 7182.94,
+      "y": 9432.95
+    },
+    {
+      "name": "Ivashevka",
+      "x": 5757.28,
+      "y": 362.06
+    },
+    {
+      "name": "Kirillovka",
+      "x": 4601.88,
+      "y": 337.5
+    },
+    {
+      "name": "Trubetchino",
+      "x": 7741.47,
+      "y": 1018.56
+    },
+    {
+      "name": "Iunost",
+      "x": 692.31,
+      "y": 2963.36
+    },
+    {
+      "name": "Malakhovo",
+      "x": 2466.02,
+      "y": 9350.91
+    },
+    {
+      "name": "Kolodkino",
+      "x": 9059.72,
+      "y": 1941.08
+    },
+    {
+      "name": "Sanino",
+      "x": 8705.67,
+      "y": 8704.46
+    },
+    {
+      "name": "Kurilovo",
+      "x": 3350.0,
+      "y": 9950.0
+    },
+    {
+      "name": "Selivanovo",
+      "x": 210.0,
+      "y": 6688.16
+    },
+    {
+      "name": "Lopukhino",
+      "x": 2608.28,
+      "y": 9738.13
+    },
+    {
+      "name": "Tolstovo",
+      "x": 9961.38,
+      "y": 2773.49
+    },
+    {
+      "name": "Fetinino",
+      "x": 2211.27,
+      "y": 9770.37
+    },
+    {
+      "name": "Kapustino",
+      "x": 7307.8,
+      "y": 62.71
+    },
+    {
+      "name": "Chizhovo",
+      "x": 1852.88,
+      "y": 9677.07
+    },
+    {
+      "name": "Svetlyi Luch",
+      "x": 2342.66,
+      "y": 10072.8
+    },
+    {
+      "name": "Nesterovo",
+      "x": 9585.72,
+      "y": 1492.22
+    },
+    {
+      "name": "Bulanovo",
+      "x": 1365.58,
+      "y": 9518.74
+    },
+    {
+      "name": "Timoshkino",
+      "x": 8547.69,
+      "y": 409.44
+    },
+    {
+      "name": "Maiskoe",
+      "x": 9072.48,
+      "y": 9420.6
+    },
+    {
+      "name": "Arbuzovo",
+      "x": 768.98,
+      "y": 9214.32
+    },
+    {
+      "name": "Zolino",
+      "x": 10185.0,
+      "y": 1745.08
+    },
+    {
+      "name": "Volovichi",
+      "x": 10017.3,
+      "y": 1324.67
+    },
+    {
+      "name": "Filippovka",
+      "x": 9612.19,
+      "y": 9428.53
+    },
+    {
+      "name": "Peschanovka",
+      "x": 9242.56,
+      "y": 9963.09
+    },
+    {
+      "name": "Kolianino",
+      "x": 9484.47,
+      "y": 10100.0
+    },
+    {
+      "name": "Senkovo",
+      "x": 389.66,
+      "y": 9887.58
+    }
+  ]
+};
